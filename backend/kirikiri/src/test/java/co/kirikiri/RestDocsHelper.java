8ff916af20637e6f1383d2accddf8d471523e7a0
2014012606 <2014012606@hanyang.ac.kr>
2023-07-10 18:58:51 +0900

    build: RestDocs 설정
    Co-authored-by: younghoondoodoom <dudgns0330@gmail.com>
    Co-authored-by: miseongk <miseongkinn@gmail.com>
    Co-authored-by: cl8d <ljwon77@daum.net>

---
+++ backend/kirikiri/src/test/java/co/kirikiri/RestDocsHelper.java
@@ -0,0 +1,39 @@
+package co.kirikiri;
+
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.documentationConfiguration;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.restdocs.RestDocumentationContextProvider;
+import org.springframework.restdocs.RestDocumentationExtension;
+import org.springframework.restdocs.mockmvc.MockMvcRestDocumentation;
+import org.springframework.restdocs.mockmvc.RestDocumentationResultHandler;
+import org.springframework.restdocs.operation.preprocess.Preprocessors;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.filter.CharacterEncodingFilter;
+
+@ExtendWith(RestDocumentationExtension.class)
+public class RestDocsHelper {
+
+    protected MockMvc mockMvc;
+    protected RestDocumentationResultHandler documentationResultHandler;
+
+    @BeforeEach
+    void setUp(final WebApplicationContext webApplicationContext,
+               final RestDocumentationContextProvider restDocumentationContextProvider) {
+        this.documentationResultHandler = MockMvcRestDocumentation.document(
+                "{class-name}/{method-name}",
+                Preprocessors.preprocessRequest(Preprocessors.prettyPrint()),
+                Preprocessors.preprocessResponse(Preprocessors.prettyPrint()));
+
+        this.mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext)
+                .addFilter(new CharacterEncodingFilter("UTF-8", true))
+                .alwaysDo(MockMvcResultHandlers.print())
+                .alwaysDo(documentationResultHandler)
+                .apply(documentationConfiguration(restDocumentationContextProvider))
+                .build();
+    }
+}
