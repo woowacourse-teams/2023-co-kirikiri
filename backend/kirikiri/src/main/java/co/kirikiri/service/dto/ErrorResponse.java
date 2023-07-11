e7b68694c9e83124743c0150f83acd8d8d5667df
ChoiYoungHoon <dudgns2865@naver.com>
2023-07-11 17:53:38 +0900

    feat: 에러 응답 객체 생성

---
+++ backend/kirikiri/src/main/java/co/kirikiri/service/dto/ErrorResponse.java
@@ -0,0 +1,11 @@
+package co.kirikiri.service.dto;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class ErrorResponse {
+
+    private String message;
+}
