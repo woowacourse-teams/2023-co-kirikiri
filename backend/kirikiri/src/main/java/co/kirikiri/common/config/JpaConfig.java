b315134fdf525b175243e29d79ca5a558d804e01
ChoiYoungHoon <dudgns2865@naver.com>
2023-07-11 17:53:55 +0900

    feat: Jpa Auditing 설정

---
+++ backend/kirikiri/src/main/java/co/kirikiri/common/config/JpaConfig.java
@@ -0,0 +1,10 @@
+package co.kirikiri.common.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@Configuration
+@EnableJpaAuditing
+public class JpaConfig {
+
+}
