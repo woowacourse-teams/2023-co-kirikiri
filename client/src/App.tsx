786e493ec93443d5efa01d9e90b7240367614a1f
sh981013s <sh981013s@gmail.com>
2023-07-12 20:26:29 +0900

    feat: responsive container 구현 및 적용

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,23 +3,26 @@ import { ThemeProvider } from 'styled-components';
 
 import GlobalStyle from '@styles/GlobalStyle';
 import theme from '@styles/theme';
+import ResponsiveContainer from '@components/@common/responsiveContainer/ResponsiveContainer';
 
 const App = () => {
   return (
     <ThemeProvider theme={theme}>
       <GlobalStyle />
-      <BrowserRouter>
-        <Routes>
-          <Route
-            path='/'
-            element={
-              <div>
-                <input />
-              </div>
-            }
-          />
-        </Routes>
-      </BrowserRouter>
+      <ResponsiveContainer>
+        <BrowserRouter>
+          <Routes>
+            <Route
+              path='/'
+              element={
+                <div>
+                  <input />
+                </div>
+              }
+            />
+          </Routes>
+        </BrowserRouter>
+      </ResponsiveContainer>
     </ThemeProvider>
   );
 };
---
+++ client/src/components/@common/responsiveContainer/ResponsiveContainer.styles.tsx
@@ -0,0 +1,22 @@
+import styled from 'styled-components';
+import BREAK_POINTS from '@constants/common/breakPoints';
+import px2vw from '@utils/common/px2vw';
+
+export const Container = styled.div`
+  width: 100%;
+  margin-right: auto;
+  margin-left: auto;
+  background: red;
+
+  @media (min-width: ${BREAK_POINTS.MOBILE}px) {
+    max-width: ${px2vw(BREAK_POINTS.MOBILE)};
+  }
+
+  @media (min-width: ${BREAK_POINTS.TABLET}px) {
+    max-width: ${px2vw(BREAK_POINTS.TABLET)};
+  }
+
+  @media (min-width: ${BREAK_POINTS.DESKTOP}px) {
+    max-width: ${px2vw(BREAK_POINTS.DESKTOP)};
+  }
+`;
