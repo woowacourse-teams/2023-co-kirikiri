cb8606224b7c1a16a738cc54a732e16aa91eb1c9
sh981013s <sh981013s@gmail.com>
2023-07-12 14:24:18 +0900

    chore: webpack 번들링 체크를 위한 코드 제거

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,29 +1,19 @@
-import styled from 'styled-components';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Counter from './Counter';
-
-const Container = styled.div`
-  position: fixed;
-  display: flex;
-`;
 
 const App = () => {
   return (
-    <Container>
-      <BrowserRouter>
-        <Routes>
-          <Route
-            path='/'
-            element={
-              <div>
-                <Counter />
-                <input />
-              </div>
-            }
-          />
-        </Routes>
-      </BrowserRouter>
-    </Container>
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path='/'
+          element={
+            <div>
+              <input />
+            </div>
+          }
+        />
+      </Routes>
+    </BrowserRouter>
   );
 };
 
