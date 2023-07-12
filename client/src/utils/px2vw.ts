18237acecfb291710978d90a4ac9f3a44319166d
sh981013s <sh981013s@gmail.com>
2023-07-12 18:08:55 +0900

    feat: parameter default value 에 BREAK_POINTS 상수 적용

--- client/src/utils/px2vw.ts
+++ client/src/utils/px2vw.ts
@@ -1,3 +1,5 @@
-const px2vw = (size: number, width = 1440) => `${(size / width) * 100}vw`;
+import BREAK_POINTS from '@constants/breakPoints';
+
+const px2vw = (size: number, width = BREAK_POINTS.DESKTOP) => `${(size / width) * 100}vw`;
 
 export default px2vw;
