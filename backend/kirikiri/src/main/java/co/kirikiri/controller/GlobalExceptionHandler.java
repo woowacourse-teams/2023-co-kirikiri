7532e6798de21601499ec718183ba83445111814
ChoiYoungHoon <dudgns2865@naver.com>
2023-07-13 13:30:30 +0900

    feat: 예외 정의 및 핸들링

---
+++ backend/kirikiri/src/main/java/co/kirikiri/controller/GlobalExceptionHandler.java
@@ -0,0 +1,48 @@
+package co.kirikiri.controller;
+
+import co.kirikiri.exception.AuthenticationException;
+import co.kirikiri.exception.BadRequestException;
+import co.kirikiri.exception.BusinessException;
+import co.kirikiri.exception.ConflictException;
+import co.kirikiri.service.dto.ErrorResponse;
+import java.util.Objects;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(BadRequestException.class)
+    public ResponseEntity<ErrorResponse> handleBadRequestException(final BadRequestException exception) {
+        final ErrorResponse errorResponse = new ErrorResponse(exception.getMessage());
+        return ResponseEntity.badRequest().body(errorResponse);
+    }
+
+    @ExceptionHandler(AuthenticationException.class)
+    public ResponseEntity<ErrorResponse> handleAuthenticationException(final AuthenticationException exception) {
+        final ErrorResponse errorResponse = new ErrorResponse(exception.getMessage());
+        return ResponseEntity.badRequest().body(errorResponse);
+    }
+
+    @ExceptionHandler(ConflictException.class)
+    public ResponseEntity<ErrorResponse> handleConflictException(final ConflictException exception) {
+        final ErrorResponse errorResponse = new ErrorResponse(exception.getMessage());
+        return ResponseEntity.badRequest().body(errorResponse);
+    }
+
+    @ExceptionHandler(BusinessException.class)
+    public ResponseEntity<ErrorResponse> handleBusinessException(final BusinessException exception) {
+        final ErrorResponse errorResponse = new ErrorResponse(exception.getMessage());
+        return ResponseEntity.badRequest().body(errorResponse);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ErrorResponse> handleMethodArgumentNotValidException(
+        final MethodArgumentNotValidException exception) {
+        final String message = Objects.requireNonNull(exception.getFieldError()).getDefaultMessage();
+        final ErrorResponse errorResponse = new ErrorResponse(message);
+        return ResponseEntity.badRequest().body(errorResponse);
+    }
+}
--- backend/kirikiri/src/main/java/co/kirikiri/exception/AuthenticationException.java
+++ backend/kirikiri/src/main/java/co/kirikiri/exception/AuthenticationException.java
@@ -1,6 +1,6 @@
 package co.kirikiri.exception;
 
-public class AuthenticationException extends RuntimeException {
+public class AuthenticationException extends BusinessException {
 
     public AuthenticationException(final String message) {
         super(message);
