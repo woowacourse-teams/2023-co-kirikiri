fd4fd04ea41439fe41691316c8b8614642838420
sh981013s <sh981013s@gmail.com>
2023-07-14 00:01:13 +0900

    feat: axios instance 에 BASE_URL 적용

--- client/src/apis/axios/client.ts
+++ client/src/apis/axios/client.ts
@@ -1,7 +1,13 @@
 import axios from 'axios';
 
+export const BASE_URL = `${
+  process.env.NODE_ENV === 'production'
+    ? process.env.PROD_SERVER
+    : process.env.API_TEST_SERVER
+}`;
+
 export const client = axios.create({
-  baseURL: ``,
+  baseURL: BASE_URL,
   headers: {
     'Content-Type': 'application/json',
   },
