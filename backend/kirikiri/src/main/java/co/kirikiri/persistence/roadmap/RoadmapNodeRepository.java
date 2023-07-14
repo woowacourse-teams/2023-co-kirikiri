73435cfae2c9d44dc91999d550dad228a20c0db5
Ohjintaek <jt950930@gmail.com>
2023-07-14 18:08:45 +0900

    feat: 특정 로드캡 콘텐츠의 노드들 조회 기능 추가

---
+++ backend/kirikiri/src/main/java/co/kirikiri/persistence/roadmap/RoadmapNodeRepository.java
@@ -0,0 +1,10 @@
+package co.kirikiri.persistence.roadmap;
+
+import co.kirikiri.domain.roadmap.RoadmapNode;
+import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RoadmapNodeRepository extends JpaRepository<RoadmapNode, Long> {
+
+    List<RoadmapNode> findByRoadmapContentId(Long roadmapContentId);
+}
