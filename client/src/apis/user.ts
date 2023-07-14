6b5f585f69221f48f1e1d53a6f5db4b4c2eea9b5
sh981013s <sh981013s@gmail.com>
2023-07-14 14:29:06 +0900

    feat: request url 에서 base url 제거

--- client/src/apis/user.ts
+++ client/src/apis/user.ts
@@ -2,7 +2,7 @@ import { client } from '@apis/axios/client';
 import type { UserResponse } from '@myTypes/user/remote';
 
 const getUser = async () => {
-  const { data } = await client.get<UserResponse>('https://reqres.in/api/users/2');
+  const { data } = await client.get<UserResponse>('/users/2');
 
   return data;
 };
