a2e6897bd283de60e8d59fb41d0ac1d9eccc341e
Ohjintaek <jt950930@gmail.com>
2023-07-14 15:46:34 +0900

    build: restAssured 의존성 추가 및 전역 코드 추가

--- backend/kirikiri/src/main/java/co/kirikiri/domain/BaseTimeEntity.java
+++ backend/kirikiri/src/main/java/co/kirikiri/domain/BaseTimeEntity.java
@@ -1,6 +1,5 @@
 package co.kirikiri.domain;
 
-
 import jakarta.persistence.Column;
 import jakarta.persistence.EntityListeners;
 import jakarta.persistence.MappedSuperclass;
--- backend/kirikiri/src/test/java/co/kirikiri/controller/helper/RestDocsHelper.java
+++ backend/kirikiri/src/test/java/co/kirikiri/controller/helper/RestDocsHelper.java
@@ -1,25 +1,35 @@
-package co.kirikiri;
+package co.kirikiri.controller.helper;
 
 import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.documentationConfiguration;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Import;
 import org.springframework.restdocs.RestDocumentationContextProvider;
 import org.springframework.restdocs.RestDocumentationExtension;
 import org.springframework.restdocs.mockmvc.MockMvcRestDocumentation;
 import org.springframework.restdocs.mockmvc.RestDocumentationResultHandler;
 import org.springframework.restdocs.operation.preprocess.Preprocessors;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 import org.springframework.web.filter.CharacterEncodingFilter;
 
+@ActiveProfiles("test")
+@Import(TestObjectMapperConfig.class)
 @ExtendWith(RestDocumentationExtension.class)
 public class RestDocsHelper {
 
+    protected static final String API_PREFIX = "/api";
+
     protected MockMvc mockMvc;
     protected RestDocumentationResultHandler documentationResultHandler;
+    @Autowired
+    protected ObjectMapper objectMapper;
 
     @BeforeEach
     void setUp(final WebApplicationContext webApplicationContext,
@@ -29,7 +39,8 @@ public class RestDocsHelper {
                 Preprocessors.preprocessRequest(Preprocessors.prettyPrint()),
                 Preprocessors.preprocessResponse(Preprocessors.prettyPrint()));
 
-        this.mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext)
+        this.mockMvc = MockMvcBuilders
+                .webAppContextSetup(webApplicationContext)
                 .addFilter(new CharacterEncodingFilter("UTF-8", true))
                 .alwaysDo(MockMvcResultHandlers.print())
                 .alwaysDo(documentationResultHandler)
