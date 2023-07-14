460714211d6000d36ccdd66c426a141fcebed64c
sh981013s <sh981013s@gmail.com>
2023-07-14 14:31:04 +0900

    feat: app.tsx에서 예시 쿼리 사용 로직 제거

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,18 +1,10 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import { useFetchUser } from '@hooks/queries/user';
-import { useEffect } from 'react';
 import theme from '@styles/theme';
 import GlobalStyle from '@styles/GlobalStyle';
 import { ThemeProvider } from 'styled-components';
 import ResponsiveContainer from '@components/_common/responsiveContainer/ResponsiveContainer';
 
 const App = () => {
-  const { user } = useFetchUser();
-
-  useEffect(() => {
-    console.log(user, 'userInfo');
-  }, [user]);
-
   return (
     <ThemeProvider theme={theme}>
       <GlobalStyle />
