818e1da3246b8867f785431f576e5850af9f16aa
sh981013s <sh981013s@gmail.com>
2023-07-14 11:53:17 +0900

    style: app.tsx, index.tsx 관심사별로 구조 변경

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,10 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { useFetchUser } from '@hooks/queries/user';
 import { useEffect } from 'react';
+import theme from '@styles/theme';
+import GlobalStyle from '@styles/GlobalStyle';
+import { ThemeProvider } from 'styled-components';
+import ResponsiveContainer from '@components/_common/responsiveContainer/ResponsiveContainer';
 
 const App = () => {
   const { user } = useFetchUser();
@@ -10,19 +14,24 @@ const App = () => {
   }, [user]);
 
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route
-          path='/'
-          element={
-            <div>
-              <p>한글이라네 자네</p>
-              <p>here comes Eng</p>
-            </div>
-          }
-        />
-      </Routes>
-    </BrowserRouter>
+    <ThemeProvider theme={theme}>
+      <GlobalStyle />
+      <ResponsiveContainer>
+        <BrowserRouter>
+          <Routes>
+            <Route
+              path='/'
+              element={
+                <div>
+                  <p>한글이라네 자네</p>
+                  <p>here comes Eng</p>
+                </div>
+              }
+            />
+          </Routes>
+        </BrowserRouter>
+      </ResponsiveContainer>
+    </ThemeProvider>
   );
 };
 
--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,9 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import theme from '@styles/theme';
-import GlobalStyle from '@styles/GlobalStyle';
-import ResponsiveContainer from '@components/_common/responsiveContainer/ResponsiveContainer';
-import { ThemeProvider } from 'styled-components';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import App from './App';
@@ -32,12 +28,7 @@ root.render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
       <ReactQueryDevtools initialIsOpen={false} />
-      <ThemeProvider theme={theme}>
-        <GlobalStyle />
-        <ResponsiveContainer>
-          <App />
-        </ResponsiveContainer>
-      </ThemeProvider>
+      <App />
     </QueryClientProvider>
   </React.StrictMode>
 );
