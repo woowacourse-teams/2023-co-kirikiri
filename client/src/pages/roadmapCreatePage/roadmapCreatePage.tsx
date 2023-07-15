3df4addd2567ceaa7edc94923f4c68ab74352cbe
NaveOWO <87578512+NaveOWO@users.noreply.github.com>
2023-07-16 07:16:14 +0900

    feat: 생성한 컴포넌트를 모아놓은 페이지 생성

---
+++ client/src/pages/roadmapCreatePage/roadmapCreatePage.tsx
@@ -0,0 +1,13 @@
+import Category from '@/components/roadmapCreatePage/category/category';
+import Difficulty from '@/components/roadmapCreatePage/difficulty/difficulty';
+
+const RoadmapCreatePage = () => {
+  return (
+    <>
+      <Category />
+      <Difficulty />
+    </>
+  );
+};
+
+export default RoadmapCreatePage;
