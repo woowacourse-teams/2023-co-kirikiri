7c55c605732a00f0a16ae056827438f2c748f8c3
sh981013s <sh981013s@gmail.com>
2023-07-17 00:39:59 +0900

    feat: 회원가입, 로그인 mutation hook 뼈대 구현

--- client/src/hooks/queries/user.ts
+++ client/src/hooks/queries/user.ts
@@ -1,11 +1,33 @@
-import { useQuery } from '@tanstack/react-query';
-import getUser from '@apis/user';
-import QUERY_KEYS from '@constants/@queryKeys/queryKeys';
+import { useMutation } from '@tanstack/react-query';
+import { MemberJoinRequest, UserLoginRequest } from '@myTypes/user/remote';
+import { login, signUp } from '@apis/user';
+import { setCookie } from '@utils/_common/cookies';
 
-export const useFetchUser = () => {
-  const { data } = useQuery([QUERY_KEYS.user.user], getUser);
+export const useSignUp = (memberJoinPayload: MemberJoinRequest) => {
+  const { mutate } = useMutation(() => signUp(memberJoinPayload), {
+    onSuccess() {
+      // TODO: 회원가입 성공 시 로직
+    },
+    onError() {
+      // TODO: 회원가입 실패 시 로직
+    },
+  });
 
   return {
-    user: data?.data,
+    signUp: mutate,
   };
+};
+
+export const useLogin = (loginPayload: UserLoginRequest) => {
+  return useMutation(() => login(loginPayload), {
+    onSuccess(response) {
+      const { accessToken, refreshToken } = response.data;
+      setCookie('access_token', accessToken);
+      setCookie('refresh_token', refreshToken);
+      alert(`로그인 성공!  ${accessToken}`);
+    },
+    onError() {
+      // TODO: 로그인 실패 시 로직
+    },
+  });
 };
