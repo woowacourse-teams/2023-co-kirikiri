fc78a2e34ba5300aa19cc35c38e5552905f65205
Miseong Kim <miseongk@naver.com>
2023-07-18 01:40:31 +0900

    refactor: 변수 추출

--- backend/kirikiri/src/main/java/co/kirikiri/service/mapper/RoadmapMapper.java
+++ backend/kirikiri/src/main/java/co/kirikiri/service/mapper/RoadmapMapper.java
@@ -4,15 +4,16 @@ import co.kirikiri.service.dto.roadmap.RoadmapNodeSaveDto;
 import co.kirikiri.service.dto.roadmap.RoadmapNodeSaveRequest;
 import co.kirikiri.service.dto.roadmap.RoadmapSaveDto;
 import co.kirikiri.service.dto.roadmap.RoadmapSaveRequest;
+import java.util.List;
 
 public class RoadmapMapper {
 
     public static RoadmapSaveDto convertToRoadmapSaveDto(final RoadmapSaveRequest request) {
+        final List<RoadmapNodeSaveDto> roadmapNodes = request.roadmapNodes().stream()
+                .map(RoadmapMapper::convertToRoadmapNodesSaveDto)
+                .toList();
         return new RoadmapSaveDto(request.categoryId(), request.title(), request.introduction(), request.content(),
-                request.difficulty(), request.requiredPeriod(),
-                request.roadmapNodes().stream()
-                        .map(RoadmapMapper::convertToRoadmapNodesSaveDto)
-                        .toList());
+                request.difficulty(), request.requiredPeriod(), roadmapNodes);
     }
 
     private static RoadmapNodeSaveDto convertToRoadmapNodesSaveDto(final RoadmapNodeSaveRequest request) {
