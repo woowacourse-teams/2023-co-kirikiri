8bda34f4d841ba6329cb690f3f5108957ecd9bf8
NaveOWO <87578512+NaveOWO@users.noreply.github.com>
2023-07-19 16:45:54 +0900

    design: mainText 스타일 코드 작성

---
+++ client/src/components/roadmapCreatePage/mainText/mainText.styles.ts
@@ -0,0 +1,24 @@
+import styled from 'styled-components';
+
+export const Container = styled.article`
+  margin-top: 6rem;
+`;
+
+export const FieldWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+
+  width: 80%;
+  height: 30rem;
+  padding-left: 2rem;
+
+  background-color: ${({ theme }) => theme.colors.gray200};
+  border-bottom: 0.2rem solid ${({ theme }) => theme.colors.gray300};
+  border-radius: 2rem;
+`;
+
+export const TextCountWrapper = styled.div`
+  height: 80%;
+  margin-right: 2rem;
+`;
