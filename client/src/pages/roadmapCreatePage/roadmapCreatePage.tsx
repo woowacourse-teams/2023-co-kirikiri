ab680d5475455bc6e3e6fb3777dbe8a9a4f4bf6c
NaveOWO <87578512+NaveOWO@users.noreply.github.com>
2023-07-19 16:51:03 +0900

    feat: 로드맵 생성 페이지 구현

--- client/src/pages/roadmapCreatePage/roadmapCreatePage.tsx
+++ client/src/pages/roadmapCreatePage/roadmapCreatePage.tsx
@@ -1,11 +1,19 @@
 import Category from '@/components/roadmapCreatePage/category/category';
+import Description from '@/components/roadmapCreatePage/description/description';
 import Difficulty from '@/components/roadmapCreatePage/difficulty/difficulty';
+import MainText from '@/components/roadmapCreatePage/mainText/mainText';
+import Period from '@/components/roadmapCreatePage/period/period';
+import Title from '@/components/roadmapCreatePage/title/title';
 
 const RoadmapCreatePage = () => {
   return (
     <>
       <Category />
+      <Title />
+      <Description />
       <Difficulty />
+      <MainText />
+      <Period />
     </>
   );
 };
