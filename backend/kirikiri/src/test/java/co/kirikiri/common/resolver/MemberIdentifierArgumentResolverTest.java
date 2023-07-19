e885ff02ac37236eee58944c8a84f2d583490653
ChoiYoungHoon <dudgns2865@naver.com>
2023-07-19 11:50:14 +0900

    test: MemberIdentifierArgumentResolver 테스트 작성

---
+++ backend/kirikiri/src/test/java/co/kirikiri/common/resolver/MemberIdentifierArgumentResolverTest.java
@@ -0,0 +1,150 @@
+package co.kirikiri.common.resolver;
+
+import co.kirikiri.exception.AuthenticationException;
+import co.kirikiri.service.AuthService;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.core.MethodParameter;
+import org.springframework.http.HttpHeaders;
+import org.springframework.web.bind.support.WebDataBinderFactory;
+import org.springframework.web.context.request.NativeWebRequest;
+import org.springframework.web.method.support.ModelAndViewContainer;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+class MemberIdentifierArgumentResolverTest {
+
+    private static final String BEARER = "Bearer ";
+
+    @Mock
+    private AuthService authService;
+
+    @Mock
+    private MethodParameter parameter;
+
+    @Mock
+    private ModelAndViewContainer mavContainer;
+
+    @Mock
+    private NativeWebRequest webRequest;
+
+    @Mock
+    private WebDataBinderFactory binderFactory;
+
+    @InjectMocks
+    private MemberIdentifierArgumentResolver memberIdentifierArgumentResolver;
+
+    @Test
+    void 정상적으로_String_타입이고_MemberIdentifier이_붙은_인자인_경우() {
+        //given
+        Mockito.<Class<?>>when(parameter.getParameterType())
+                .thenReturn(String.class);
+        when(parameter.hasParameterAnnotation(MemberIdentifier.class))
+                .thenReturn(true);
+
+        //when
+        final boolean result = memberIdentifierArgumentResolver.supportsParameter(parameter);
+
+        //then
+        assertThat(result).isTrue();
+    }
+
+    @Test
+    void String_타입이_붙지_않은_경우() {
+        //given
+        Mockito.<Class<?>>when(parameter.getParameterType())
+                .thenReturn(List.class);
+
+        //when
+        final boolean result = memberIdentifierArgumentResolver.supportsParameter(parameter);
+
+        //then
+        assertThat(result).isFalse();
+    }
+
+    @Test
+    void String_타입이고_MemberIdentifier이_붙지_않은_인자인_경우() {
+        //given
+        Mockito.<Class<?>>when(parameter.getParameterType())
+                .thenReturn(String.class);
+        when(parameter.hasParameterAnnotation(MemberIdentifier.class))
+                .thenReturn(false);
+
+        //when
+        final boolean result = memberIdentifierArgumentResolver.supportsParameter(parameter);
+
+        //then
+        assertThat(result).isFalse();
+    }
+
+    @Test
+    void 정상적으로_토큰이_들어온_경우_토큰을_인증한다() {
+        // given
+        final String expectedIdentifier = "test";
+        final String token = "testToken";
+
+        when(webRequest.getHeader(HttpHeaders.AUTHORIZATION))
+                .thenReturn(BEARER + token);
+        when(authService.findIdentifierByToken(anyString()))
+                .thenReturn(expectedIdentifier);
+
+        // when
+        final String actualIdentifier = memberIdentifierArgumentResolver.resolveArgument(parameter, mavContainer, webRequest, binderFactory);
+
+        // then
+        assertThat(actualIdentifier).isEqualTo(expectedIdentifier);
+    }
+
+    @Test
+    void AUTHORIZATION_HEADER에_BEARER이_안붙은_경우_토큰을_인증한다() {
+        // given
+        final String token = "testToken";
+
+        when(webRequest.getHeader(HttpHeaders.AUTHORIZATION))
+                .thenReturn(token);
+
+        // when
+        // then
+        assertThatThrownBy(() -> memberIdentifierArgumentResolver.resolveArgument(parameter, mavContainer, webRequest, binderFactory))
+                .isInstanceOf(AuthenticationException.class);
+
+    }
+
+    @Test
+    void AUTHORIZATION_HEADER가_비어있을_경우_토큰을_인증한다() {
+        // given
+        when(webRequest.getHeader(HttpHeaders.AUTHORIZATION))
+                .thenReturn(null);
+
+        // when
+        // then
+        assertThatThrownBy(() -> memberIdentifierArgumentResolver.resolveArgument(parameter, mavContainer, webRequest, binderFactory))
+                .isInstanceOf(AuthenticationException.class);
+    }
+
+    @Test
+    void 적절하지_않은_토큰이_들어올_경우_토큰을_인증한다() {
+        // given
+        final String token = "testToken";
+
+        when(webRequest.getHeader(HttpHeaders.AUTHORIZATION))
+                .thenReturn(BEARER + token);
+        when(authService.findIdentifierByToken(anyString()))
+                .thenThrow(new AuthenticationException("인증 헤더가 적절하지 않습니다."));
+
+        // when
+        // then
+        assertThatThrownBy(() -> memberIdentifierArgumentResolver.resolveArgument(parameter, mavContainer, webRequest, binderFactory))
+                .isInstanceOf(AuthenticationException.class);
+    }
+}
