7d3f4ff3e87b89af89e0b4bd866f9723932117f3
NaveOWO <87578512+NaveOWO@users.noreply.github.com>
2023-07-19 16:43:38 +0900

    design: inputField 스타일 코드 작성

---
+++ client/src/components/roadmapCreatePage/input/inputField/inputField.styles.ts
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+export const Input = styled.textarea`
+  width: 80%;
+  height: 80%;
+  color: ${({ theme }) => theme.colors.gray300};
+  &::placeholder {
+    color: ${({ theme }) => theme.colors.gray200};
+    ${({ theme }) => theme.fonts.description1};
+  }
+`;
