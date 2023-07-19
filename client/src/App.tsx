25089159afeae77a071d0a36c1bca0afe4b566f5
sh981013s <sh981013s@gmail.com>
2023-07-19 14:09:22 +0900

    feat: pageLayout HOC component 적용

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,31 +3,34 @@ import theme from '@styles/theme';
 import GlobalStyle from '@styles/GlobalStyle';
 import { ThemeProvider } from 'styled-components';
 import ResponsiveContainer from '@components/_common/responsiveContainer/ResponsiveContainer';
-import RoadmapCreatePage from './pages/roadmapCreatePage/roadmapCreatePage';
 import SignUpPage from '@pages/signUpPage/SignUpPage';
 import LoginPage from '@pages/loginPage/LoginPage';
+import PageLayout from '@components/_common/pageLayout/PageLayout';
+import RoadmapCreatePage from './pages/roadmapCreatePage/roadmapCreatePage';
 
 const App = () => {
   return (
     <ThemeProvider theme={theme}>
       <GlobalStyle />
       <ResponsiveContainer>
-        <BrowserRouter>
-          <Routes>
-            <Route
-              path='/'
-              element={
-                <div>
-                  <p>한글이라네 자네</p>
-                  <p>here comes Eng</p>
-                </div>
-              }
-            />
-            <Route path='/roadmap-create' element={<RoadmapCreatePage />} />
-            <Route path='/login' element={<LoginPage />} />
-            <Route path='/join' element={<SignUpPage />} />
-          </Routes>
-        </BrowserRouter>
+        <PageLayout>
+          <BrowserRouter>
+            <Routes>
+              <Route
+                path='/'
+                element={
+                  <div>
+                    <p>한글이라네 자네</p>
+                    <p>here comes Eng</p>
+                  </div>
+                }
+              />
+              <Route path='/roadmap-create' element={<RoadmapCreatePage />} />
+              <Route path='/login' element={<LoginPage />} />
+              <Route path='/join' element={<SignUpPage />} />
+            </Routes>
+          </BrowserRouter>
+        </PageLayout>
       </ResponsiveContainer>
     </ThemeProvider>
   );
