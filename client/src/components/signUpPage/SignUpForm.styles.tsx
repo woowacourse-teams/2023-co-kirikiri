d29586b7013edb1b8239da81e5afa9ff7fb9516f
sh981013s <sh981013s@gmail.com>
2023-07-19 14:18:00 +0900

    feat: 회원가입 페이지 구현

---
+++ client/src/components/signUpPage/SignUpForm.styles.tsx
@@ -0,0 +1,62 @@
+import styled from 'styled-components';
+import { Link } from 'react-router-dom';
+
+export const FormList = styled.div`
+  width: 100%;
+  margin-top: 2rem;
+  border: 1px solid ${({ theme }) => theme.colors.gray300};
+  border-radius: 6px;
+`;
+
+export const FormItem = styled.div`
+  pointer-events: none;
+
+  display: flex;
+  align-items: center;
+
+  min-height: 5rem;
+  padding: 0 1rem 0 1rem;
+
+  border: 1px solid ${({ theme }) => theme.colors.gray300};
+
+  input,
+  select {
+    pointer-events: auto;
+    width: 100%;
+    margin-left: 1rem;
+    border: none;
+  }
+`;
+
+export const InfoText = styled.p<{ centered?: boolean }>`
+  ${({ theme }) => theme.fonts.description2};
+  margin-top: 1rem;
+  text-align: ${({ centered }) => (centered ? 'center' : 'left')};
+`;
+
+export const BoldText = styled.span`
+  font-weight: 700;
+`;
+
+export const SubmitButton = styled.button`
+  width: 100%;
+  height: 3rem;
+  margin-top: 1rem;
+  padding: 1rem;
+
+  font-size: 1.125rem;
+  font-weight: 700;
+  color: ${({ theme }) => theme.colors.white};
+
+  background-color: ${({ theme }) => theme.colors.main_dark};
+  border: none;
+  border-radius: 6px;
+`;
+
+export const StyledLink = styled(Link)`
+  color: ${({ theme }) => theme.colors.main_dark};
+
+  &:hover {
+    text-decoration: underline;
+  }
+`;
--- client/src/components/signUpPage/SignUpForm.tsx
+++ client/src/components/signUpPage/SignUpForm.tsx
@@ -1,64 +1,95 @@
-import { ChangeEvent, FormEvent, useState } from 'react';
+import { FormEvent } from 'react';
 import { MemberJoinRequest } from '@myTypes/user/remote';
 import { useSignUp } from '@hooks/queries/user';
+import SVGIcon from '@components/icons/SVGIcon';
+import logo from '@assets/images/logo.png';
+import { SingleCardWrapper } from '@components/_common/SingleCard/SingleCard.styles';
+import useFormInput from '@hooks/_common/useFormInput';
+import * as S from './SignUpForm.styles';
 
 const SignUpForm = () => {
-  const [signUpFormData, setSignUpFormData] = useState<MemberJoinRequest>({
-    identifier: '',
-    password: '',
-    nickname: '',
-    phoneNumber: '',
-    genderType: '',
-    birthDate: '',
-  });
-
-  const { signUp } = useSignUp(signUpFormData);
-
-  const handleInputChange = (
-    event: ChangeEvent<HTMLInputElement | HTMLSelectElement>
-  ) => {
-    setSignUpFormData({
-      ...signUpFormData,
-      [event.target.name]: event.target.value,
+  const { formState: signUpFormData, handleInputChange } =
+    useFormInput<MemberJoinRequest>({
+      identifier: '',
+      password: '',
+      nickname: '',
+      phoneNumber: '',
+      genderType: '',
+      birthDate: '',
     });
-  };
 
-  const handleSubmit = async (event: FormEvent<HTMLFormElement>) => {
+  const { signUp } = useSignUp();
+
+  const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
     event.preventDefault();
-    await signUp();
+    signUp(signUpFormData);
   };
 
   return (
-    <div>
-      <h1>SignUpPage</h1>
+    <SingleCardWrapper>
+      <h1>
+        <img src={logo} alt='코끼리 로고' />
+      </h1>
       <form onSubmit={handleSubmit}>
-        <input name='identifier' onChange={handleInputChange} placeholder='Identifier' />
-        <input
-          name='password'
-          onChange={handleInputChange}
-          placeholder='Password'
-          type='password'
-        />
-        <input name='nickname' onChange={handleInputChange} placeholder='Nickname' />
-        <input
-          name='phoneNumber'
-          onChange={handleInputChange}
-          placeholder='Phone Number'
-        />
-        <select name='genderType' onChange={handleInputChange}>
-          <option value=''>Select Gender</option>
-          <option value='male'>Male</option>
-          <option value='female'>Female</option>
-        </select>
-        <input
-          name='birthDate'
-          onChange={handleInputChange}
-          placeholder='Birth Date'
-          type='date'
-        />
-        <button type='submit'>Sign Up</button>
+        <S.FormList>
+          <S.FormItem>
+            <SVGIcon name='PersonIcon' />
+            <input name='identifier' onChange={handleInputChange} placeholder='아이디' />
+          </S.FormItem>
+          <S.FormItem>
+            <SVGIcon name='LockIcon' />
+            <input
+              name='password'
+              onChange={handleInputChange}
+              placeholder='비밀번호'
+              type='password'
+            />
+          </S.FormItem>
+          <S.FormItem>
+            <SVGIcon name='StandingPersonIcon' />
+            <input name='nickname' onChange={handleInputChange} placeholder='닉네임' />
+          </S.FormItem>
+        </S.FormList>
+
+        <S.FormList>
+          <S.FormItem>
+            <SVGIcon name='PhoneIcon' />
+            <input
+              name='phoneNumber'
+              onChange={handleInputChange}
+              placeholder='휴대전화번호 ex) 01012345678'
+            />
+          </S.FormItem>
+          <S.FormItem>
+            <SVGIcon name='GenderIcon' />
+            <select name='genderType' onChange={handleInputChange}>
+              <option value='' selected disabled>
+                성별
+              </option>
+              <option value='male'>남성</option>
+              <option value='female'>여성</option>
+            </select>
+          </S.FormItem>
+          <S.FormItem>
+            <SVGIcon name='CalendarIcon' />
+            <input
+              name='birthDate'
+              onChange={handleInputChange}
+              placeholder='생년월일 6자리 ex) 950101'
+              type='number'
+            />
+          </S.FormItem>
+        </S.FormList>
+        <S.InfoText>
+          가입을 진행하시는 것은 우리의 <S.BoldText>이용 약관</S.BoldText> 및{' '}
+          <S.BoldText>개인정보 보호 정책</S.BoldText>에 동의하신 것으로 간주됩니다.
+        </S.InfoText>
+        <S.SubmitButton>회원가입</S.SubmitButton>
+        <S.InfoText centered>
+          이미 계정이 있습니까? <S.StyledLink to='/login'>로그인하기</S.StyledLink>
+        </S.InfoText>
       </form>
-    </div>
+    </SingleCardWrapper>
   );
 };
 
