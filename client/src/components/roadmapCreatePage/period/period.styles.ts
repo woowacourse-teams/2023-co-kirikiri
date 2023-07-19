f2dd4d3ced0891ab4f6474a00e52fdc308b227b7
NaveOWO <87578512+NaveOWO@users.noreply.github.com>
2023-07-19 16:46:06 +0900

    design: period 스타일 코드 작성

---
+++ client/src/components/roadmapCreatePage/period/period.styles.ts
@@ -0,0 +1,26 @@
+import styled from 'styled-components';
+
+export const Container = styled.article`
+  margin-top: 6rem;
+`;
+
+export const FieldWrapper = styled.div`
+  display: flex;
+  justify-content: space-between;
+
+  width: 7rem;
+  height: 2.5rem;
+  padding-left: 1rem;
+
+  border-bottom: 0.2rem solid ${({ theme }) => theme.colors.gray300};
+  ${({ theme }) => theme.fonts.nav_title}
+
+  > p {
+    padding-bottom: 1rem;
+  }
+`;
+
+export const ErrorMessage = styled.div`
+  height: 2rem;
+  color: ${({ theme }) => theme.colors.red};
+`;
