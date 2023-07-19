0b42aa7ba1af3414cabaf0b0fb3a96f66e928f6e
NaveOWO <87578512+NaveOWO@users.noreply.github.com>
2023-07-19 16:47:38 +0900

    feat: 본문 입력 컴포넌트 생성

---
+++ client/src/components/roadmapCreatePage/mainText/mainText.tsx
@@ -0,0 +1,31 @@
+import { useValidateInput } from '@/hooks/_common/useValidateInput';
+import InputField from '../input/inputField/inputField';
+import InputLabel from '../input/inputLabel/inputLebel';
+import TextCount from '../input/textCount/textCount';
+import * as S from './mainText.styles';
+
+const MAIN_TEXT_MAX_LENGTH = { rule: /^.{0,2000}$/, message: '' };
+const MainText = () => {
+  const { handleInputChange, checkBlank, value } = useValidateInput([
+    MAIN_TEXT_MAX_LENGTH,
+  ]);
+
+  return (
+    <S.Container>
+      <InputLabel text='본문' />
+      <S.FieldWrapper>
+        <InputField
+          placeholder='컨텐츠를 자세하게 설명해주세요'
+          handleInputChange={handleInputChange}
+          maxLength={2000}
+          checkBlank={checkBlank}
+        />
+        <S.TextCountWrapper>
+          <TextCount maxCount={2000} currentCount={value.length} />
+        </S.TextCountWrapper>
+      </S.FieldWrapper>
+    </S.Container>
+  );
+};
+
+export default MainText;
