f56c3ee0fa6cbb4e1e155abd71010b5d8eafa8fb
NaveOWO <87578512+NaveOWO@users.noreply.github.com>
2023-07-19 16:40:25 +0900

    chore: lint규칙 추가

--- client/.eslintrc.cjs
+++ client/.eslintrc.cjs
@@ -21,28 +21,28 @@ module.exports = {
     node: true,
   },
   settings: {
-    "import/resolver": {
-      "node": {
-        "extensions": [".js", ".jsx", ".ts", ".tsx"],
-        "paths": ["src"]
+    'import/resolver': {
+      node: {
+        extensions: ['.js', '.jsx', '.ts', '.tsx'],
+        paths: ['src'],
       },
-      "alias": {
-        "map": [
-          ["@", "./src"],
-          ["@apis", "./src/apis"],
-          ["@assets", "./src/assets"],
-          ["@components", "./src/components"],
-          ["@constants", "./src/constants"],
-          ["@hooks", "./src/hooks"],
-          ["@mocks", "./src/mocks"],
-          ["@pages", "./src/pages"],
-          ["@styles", "./src/styles"],
-          ["@myTypes", "./src/myTypes"],
-          ["@utils", "./src/utils"]
+      alias: {
+        map: [
+          ['@', './src'],
+          ['@apis', './src/apis'],
+          ['@assets', './src/assets'],
+          ['@components', './src/components'],
+          ['@constants', './src/constants'],
+          ['@hooks', './src/hooks'],
+          ['@mocks', './src/mocks'],
+          ['@pages', './src/pages'],
+          ['@styles', './src/styles'],
+          ['@myTypes', './src/myTypes'],
+          ['@utils', './src/utils'],
         ],
-        "extensions": [".ts", ".tsx", ".js", ".jsx", ".json"]
-      }
-    }
+        extensions: ['.ts', '.tsx', '.js', '.jsx', '.json'],
+      },
+    },
   },
   rules: {
     'import/extensions': [
@@ -58,13 +58,13 @@ module.exports = {
     'prettier/prettier': 'error',
     'react/react-in-jsx-scope': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
-    "react/jsx-filename-extension": [
+    'react/jsx-filename-extension': [
       1,
       {
-        "extensions": [".js", ".jsx", ".ts", ".tsx"]
-      }
+        extensions: ['.js', '.jsx', '.ts', '.tsx'],
+      },
     ],
-    "space-before-blocks": ["error", "always"],
+    'space-before-blocks': ['error', 'always'],
     'react/button-has-type': 'off',
     '@typescript-eslint/no-unused-vars': [
       'warn',
@@ -91,12 +91,14 @@ module.exports = {
     ],
     'react-hooks/exhaustive-deps': 'off',
     'no-shadow': 'off',
-    'import/prefer-default-export': 'warn',
-    "global-require": "warn",
-    "import/no-extraneous-dependencies": "off",
+    'import/prefer-default-export': 'off',
+    'global-require': 'warn',
+    'import/no-extraneous-dependencies': 'off',
     'no-use-before-define': 'off',
-    '@typescript-eslint/no-use-before-define': ['error', {"ignoreTypeReferences": true}],
+    '@typescript-eslint/no-use-before-define': ['error', { ignoreTypeReferences: true }],
     'no-plusplus': 'off',
     'no-param-reassign': 'off',
+    'react/require-default-props': 'off',
+    'react/jsx-no-constructed-context-values': 'off',
   },
 };
