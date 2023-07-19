cf81fe1e20f3c43ffad9d8fa5feb313374c45c86
NaveOWO <87578512+NaveOWO@users.noreply.github.com>
2023-07-20 03:02:50 +0900

    design: PageLayout 컴포넌트 적용

--- client/src/pages/roadmapCreatePage/roadmapCreatePage.tsx
+++ client/src/pages/roadmapCreatePage/roadmapCreatePage.tsx
@@ -4,17 +4,18 @@ import Difficulty from '@/components/roadmapCreatePage/difficulty/difficulty';
 import MainText from '@/components/roadmapCreatePage/mainText/mainText';
 import Period from '@/components/roadmapCreatePage/period/period';
 import Title from '@/components/roadmapCreatePage/title/title';
+import PageLayout from '@/components/_common/pageLayout/PageLayout';
 
 const RoadmapCreatePage = () => {
   return (
-    <>
+    <PageLayout>
       <Category />
       <Title />
       <Description />
       <Difficulty />
       <MainText />
       <Period />
-    </>
+    </PageLayout>
   );
 };
 
