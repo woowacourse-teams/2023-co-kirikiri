ca0f1339ef5e1ea4f7741025ccbcad27d3040ca0
NaveOWO <87578512+NaveOWO@users.noreply.github.com>
2023-07-19 16:47:13 +0900

    feat: 소개글 입력 컴포넌트 생성

---
+++ client/src/components/roadmapCreatePage/description/description.tsx
@@ -0,0 +1,31 @@
+import { useValidateInput } from '@/hooks/_common/useValidateInput';
+import InputField from '../input/inputField/inputField';
+import InputLabel from '../input/inputLabel/inputLebel';
+import TextCount from '../input/textCount/textCount';
+import * as S from './description.styles';
+
+const DESCRIPTION_MAX_LENGTH = { rule: /^.{0,150}$/, message: '소개글을 입력해주세요' };
+
+const Description = () => {
+  const { handleInputChange, checkBlank, errorMessage, value } = useValidateInput([
+    DESCRIPTION_MAX_LENGTH,
+  ]);
+
+  return (
+    <S.Container>
+      <InputLabel text='소개글' />
+      <S.FieldWrapper>
+        <InputField
+          placeholder='컨텐츠를 소개하는 문장을 작성해주세요'
+          handleInputChange={handleInputChange}
+          maxLength={150}
+          checkBlank={checkBlank}
+        />
+        <TextCount maxCount={150} currentCount={value.length} />
+      </S.FieldWrapper>
+      <S.ErrorMessage>{errorMessage}</S.ErrorMessage>
+    </S.Container>
+  );
+};
+
+export default Description;
