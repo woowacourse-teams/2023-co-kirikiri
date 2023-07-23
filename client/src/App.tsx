ac4ec7a68c7f915cf579e322036fa0f366a1e72b
Jungwoo <evencoding@gmail.com>
2023-07-23 14:40:48 +0900

    feat: Fallback 컴포넌트 구현 및 적용

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,3 +1,4 @@
+import { Suspense } from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import theme from '@styles/theme';
 import GlobalStyle from '@styles/GlobalStyle';
@@ -6,10 +7,12 @@ import ResponsiveContainer from '@components/_common/responsiveContainer/Respons
 import SignUpPage from '@pages/signUpPage/SignUpPage';
 import LoginPage from '@pages/loginPage/LoginPage';
 import PageLayout from '@components/_common/pageLayout/PageLayout';
-import RoadmapListPage from '@pages/roadmapListPage/roadmapListPage';
+import RoadmapListPage from '@pages/roadmapListPage/RoadmapListPage';
 import GoalRoomListPage from '@components/goalRommListPage/GoalRoomListPage';
 import MainPage from '@components/mainPage/MainPage';
+import Fallback from '@components/_common/fallback/Fallback';
 import RoadmapCreatePage from './pages/roadmapCreatePage/roadmapCreatePage';
+import RoadmapDetailPage from './pages/roadmapDetailPage/RoadmapDetailPage';
 
 const App = () => {
   return (
@@ -23,6 +26,14 @@ const App = () => {
               <Route path='/login' element={<LoginPage />} />
               <Route path='/join' element={<SignUpPage />} />
               <Route path='/roadmap-list' element={<RoadmapListPage />} />
+              <Route
+                path='/roadmap/:id'
+                element={
+                  <Suspense fallback={<Fallback />}>
+                    <RoadmapDetailPage />
+                  </Suspense>
+                }
+              />
               <Route path='/roadmap-create' element={<RoadmapCreatePage />} />
               <Route path='/goalroom-list' element={<GoalRoomListPage />} />
             </Routes>
