b3951e47ee61d22f725a1174d94f050b014b3eb0
Jungwoo <evencoding@gmail.com>
2023-07-23 14:42:03 +0900

    design: 로드맵의 body가 ChilrenLayout의 width의 100%를 적용

--- client/src/components/roadmapDetailPage/roadmapBody/RoadmapBody.styles.ts
+++ client/src/components/roadmapDetailPage/roadmapBody/RoadmapBody.styles.ts
@@ -1,7 +1,16 @@
+import media from '@styles/media';
 import { styled } from 'styled-components';
 
-export const RoadmapBody = styled.div`
+export const RoadmapBody = styled.p`
+  ${({ theme }) => theme.fonts.description5}
   width: 100%;
+  margin: 4rem 0;
+  padding: 4rem 8rem;
+
   border-radius: 3rem;
-  box-shadow: ${({ theme }) => theme.shadows.boxBig};
+  box-shadow: ${({ theme }) => theme.shadows.box};
+
+  ${media.mobile`
+    padding: 4rem 4rem;
+  `}
 `;
