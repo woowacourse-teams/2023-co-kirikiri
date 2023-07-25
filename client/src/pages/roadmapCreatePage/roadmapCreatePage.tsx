fa77dcb3dac677d27da7061a82feac5ce4795bbc
NaveOWO <87578512+NaveOWO@users.noreply.github.com>
2023-07-26 03:31:03 +0900

    feat: 페이지 내부 컴포넌트를 form으로 통합

--- client/src/pages/roadmapCreatePage/roadmapCreatePage.tsx
+++ client/src/pages/roadmapCreatePage/roadmapCreatePage.tsx
@@ -1,23 +1,7 @@
-import Category from '@/components/roadmapCreatePage/category/category';
-import Description from '@/components/roadmapCreatePage/description/description';
-import Difficulty from '@/components/roadmapCreatePage/difficulty/difficulty';
-import MainText from '@/components/roadmapCreatePage/mainText/mainText';
-import Period from '@/components/roadmapCreatePage/period/period';
-import Roadmap from '@/components/roadmapCreatePage/roadmap/roadmap';
-import Title from '@/components/roadmapCreatePage/title/title';
+import RoadmapCreateForm from '@/components/roadmapCreatePage/roadmapCreateForm/RoadmapCreateForm';
 
 const RoadmapCreatePage = () => {
-  return (
-    <>
-      <Category />
-      <Title />
-      <Description />
-      <Difficulty />
-      <MainText />
-      <Period />
-      <Roadmap />
-    </>
-  );
+  return <RoadmapCreateForm />;
 };
 
 export default RoadmapCreatePage;
