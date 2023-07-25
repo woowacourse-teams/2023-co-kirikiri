910d9dcce005e1b1071cb03f527949b8eb6af455
sh981013s <sh981013s@gmail.com>
2023-07-25 15:24:15 +0900

    design: 골룸 헤더 컴포넌트 스타일링 적용

--- client/src/components/goalRoomDahsboardPage/goalRoomDashboardHeader/GoalRoomDashboardHeader.tsx
+++ client/src/components/goalRoomDahsboardPage/goalRoomDashboardHeader/GoalRoomDashboardHeader.tsx
@@ -1,17 +1,19 @@
 import SVGIcon from '@components/icons/SVGIcon';
 
+import * as S from './GoalRoomDashboardHeader.styles';
+
 const GoalRoomDashboardHeader = () => {
   return (
     <header>
-      <h1>hello</h1>
-      <div>
+      <S.GoalRoomDashboardTitle>hello</S.GoalRoomDashboardTitle>
+      <S.GoalRoomLabel>
         <SVGIcon name='PersonIcon' />
         <span>25 / 30 명 참여 중</span>
-      </div>
-      <div>
+      </S.GoalRoomLabel>
+      <S.GoalRoomLabel>
         <SVGIcon name='CalendarIcon' />
         <span>2023.03.12 ~ 2023.04.21</span>
-      </div>
+      </S.GoalRoomLabel>
     </header>
   );
 };
