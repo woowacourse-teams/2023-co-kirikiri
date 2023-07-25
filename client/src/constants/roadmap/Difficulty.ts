0950b7db4b5b214d5fbbc5cf3f7f8be254a059d8
Jungwoo <evencoding@gmail.com>
2023-07-25 17:32:49 +0900

    chore: Git 캐시 삭제

--- client/src/constants/roadmap/Difficulty.ts
+++
@@ -1,7 +0,0 @@
-export const DIFFICULTY_ICON_NAME = {
-  VERY_EASY: 'VeryEasyIcon',
-  EASY: 'EasyIcon',
-  NORMAL: 'NormalIcon',
-  DIFFICULT: 'difficultIcon',
-  VERY_DIFFICULT: 'VeryDifficultIcon',
-} as const;
