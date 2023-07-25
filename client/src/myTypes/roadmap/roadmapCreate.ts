93dd7af2b5cbd0916cb05c152fba4eab0b903922
NaveOWO <87578512+NaveOWO@users.noreply.github.com>
2023-07-26 03:30:31 +0900

    feat: RaodmapValueType타입 추가

--- client/src/myTypes/roadmap/roadmapCreate.ts
+++ client/src/myTypes/roadmap/roadmapCreate.ts
@@ -2,3 +2,13 @@ export type PatternType = {
   rule: RegExp;
   message: string;
 };
+
+export type RaodmapValueType = {
+  categoryId: null | number;
+  title: null | string;
+  introduction: null | string;
+  content: null | string;
+  difficulty: null | number;
+  requiredPeriod: null | string;
+  roadmapNodes: [];
+};
