5ef7b746b310393d2121671ec4ef38dc04cca405
sh981013s <sh981013s@gmail.com>
2023-07-25 17:03:16 +0900

    design: 대시보드 요소 스타일링 고도화

--- client/src/components/goalRoomDahsboardPage/goalRoomDashboardContent/GoalRoomDashboardContent.styles.ts
+++ client/src/components/goalRoomDahsboardPage/goalRoomDashboardContent/GoalRoomDashboardContent.styles.ts
@@ -2,16 +2,35 @@ import styled from 'styled-components';
 
 export const GoalRoomGridContainer = styled.div`
   display: grid;
-  grid-template-columns: repeat(auto-fit, minmax(250px, 1fr));
-  grid-template-rows: repeat(250px, 1fr);
-  gap: 20px;
+  grid-template-columns: repeat(auto-fit, minmax(28rem, 1fr));
+  grid-template-rows: repeat(28rem, 1fr);
+  gap: 2rem;
 
   min-height: 90vh;
-  padding: 20px;
+  margin-top: 1.5rem;
+  padding: 1rem 0;
 `;
 
 export const DashBoardSection = styled.section`
   min-height: 250px;
-  padding: 20px;
+  padding: 1rem;
+  border: 0.5rem solid ${({ theme }) => theme.colors.gray200};
   border-radius: 5px;
+
+  & > div:first-child {
+    ${({ theme }) => theme.fonts.h1};
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+
+    & > button {
+      ${({ theme }) => theme.fonts.button2};
+      display: flex;
+      align-items: center;
+
+      & > svg {
+        width: 1.8rem;
+      }
+    }
+  }
 `;
