25ff2b52a5c744399c2ad1eb1917df46c2bf68f0
sh981013s <sh981013s@gmail.com>
2023-07-31 20:44:14 +0900

    feat: 골룸 dashboard fetching api function 네이밍 getGoalRoomDashboard로 변경

--- client/src/apis/goalRoom.ts
+++ client/src/apis/goalRoom.ts
@@ -1,7 +1,7 @@
 import client from '@apis/axios/client';
 import { GoalRoomBrowseResponse } from '@myTypes/goalRoom/remote';
 
-export const browseGoalRoom = async (goalRoomId: string) => {
+export const getGoalRoomDashboard = async (goalRoomId: string) => {
   const { data } = await client.get<GoalRoomBrowseResponse>(
     `/api/members/goal-rooms/${goalRoomId}`
   );
--- client/src/hooks/queries/goalRoom.ts
+++ client/src/hooks/queries/goalRoom.ts
@@ -1,9 +1,9 @@
-import { browseGoalRoom } from '@apis/goalRoom';
+import { getGoalRoomDashboard } from '@apis/goalRoom';
 import { useSuspendedQuery } from '@hooks/queries/useSuspendedQuery';
 
 export const useFetchGoalRoom = (goalRoomId: string) => {
   const { data } = useSuspendedQuery(['goalRoom', goalRoomId], () =>
-    browseGoalRoom(goalRoomId)
+    getGoalRoomDashboard(goalRoomId)
   );
 
   return {
