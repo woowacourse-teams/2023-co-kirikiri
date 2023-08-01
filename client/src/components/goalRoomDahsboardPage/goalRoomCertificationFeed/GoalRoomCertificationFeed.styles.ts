dd42b2a22612810ffaa9d3f453f9dd2ab48416eb
sh981013s <sh981013s@gmail.com>
2023-08-02 01:45:21 +0900

    design: 골룸 피드 스타일 구현

--- client/src/components/goalRoomDahsboardPage/goalRoomCertificationFeed/GoalRoomCertificationFeed.styles.ts
+++ client/src/components/goalRoomDahsboardPage/goalRoomCertificationFeed/GoalRoomCertificationFeed.styles.ts
@@ -2,3 +2,28 @@ import { DashBoardSection } from '@components/goalRoomDahsboardPage/goalRoomDash
 import styled from 'styled-components';
 
 export const CertificationFeedWrapper = styled(DashBoardSection)``;
+
+export const ImageGrid = styled.div`
+  display: grid;
+  grid-gap: 1rem;
+  grid-template-columns: repeat(2, 1fr);
+  grid-template-rows: repeat(2, 1fr);
+
+  max-width: 50rem;
+  max-height: 50rem;
+  margin: 0 auto;
+`;
+
+export const StyledImage = styled.img`
+  width: 100%;
+  height: 100%;
+
+  object-fit: cover;
+  box-shadow: 0 1px 3px rgba(0, 0, 0, 0.12), 0 1px 2px rgba(0, 0, 0, 0.24);
+
+  transition: all 0.3s cubic-bezier(0.25, 0.8, 0.25, 1);
+
+  &:hover {
+    box-shadow: 0 14px 28px rgba(0, 0, 0, 0.25), 0 10px 10px rgba(0, 0, 0, 0.22);
+  }
+`;
