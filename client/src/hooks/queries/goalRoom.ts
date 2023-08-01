f6c99dfb07cc0f04b358b307682ec8a47fb0f835
Jungwoo <evencoding@gmail.com>
2023-08-02 01:41:08 +0900

    feat: 골룸 생성 POST 요청 구현

--- client/src/hooks/queries/goalRoom.ts
+++ client/src/hooks/queries/goalRoom.ts
@@ -1,5 +1,7 @@
-import { getGoalRoomDashboard } from '@apis/goalRoom';
+import { CreateGoalRoomRequest } from '@myTypes/goalRoom/remote';
+import { createGoalRoom, getGoalRoomDashboard } from '@apis/goalRoom';
 import { useSuspendedQuery } from '@hooks/queries/useSuspendedQuery';
+import { useMutation } from '@tanstack/react-query';
 
 export const useFetchGoalRoom = (goalRoomId: string) => {
   const { data } = useSuspendedQuery(['goalRoom', goalRoomId], () =>
@@ -8,5 +10,16 @@ export const useFetchGoalRoom = (goalRoomId: string) => {
 
   return {
     goalRoom: data,
+  };
+};
+
+export const useCreateGoalRoom = () => {
+  const { mutate } = useMutation((body: CreateGoalRoomRequest) => createGoalRoom(body), {
+    onSuccess() {},
+    onError() {},
+  });
+
+  return {
+    createGoalRoom: mutate,
   };
 };
