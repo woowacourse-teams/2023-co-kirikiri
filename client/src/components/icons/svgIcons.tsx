93fc7aee6a7e40fee986908f8673991be36b5c9a
sh981013s <sh981013s@gmail.com>
2023-08-01 18:49:51 +0900

    feat: 투두리스트 아이콘 추가

--- client/src/components/icons/svgIcons.tsx
+++ client/src/components/icons/svgIcons.tsx
@@ -2365,3 +2365,33 @@ export const RightArrowIcon = ({ width, ...props }: SVGProps<SVGSVGElement>) => 
     />
   </svg>
 );
+
+export const EmptyCircle = ({ width, ...props }: SVGProps<SVGSVGElement>) => (
+  <svg
+    {...props}
+    xmlns='http://www.w3.org/2000/svg'
+    width={width}
+    height={width}
+    viewBox='0 0 256 256'
+  >
+    <path
+      fill='currentColor'
+      d='M128 26a102 102 0 1 0 102 102A102.12 102.12 0 0 0 128 26Zm0 192a90 90 0 1 1 90-90a90.1 90.1 0 0 1-90 90Z'
+    />
+  </svg>
+);
+
+export const CheckedCircle = ({ width, ...props }: SVGProps<SVGSVGElement>) => (
+  <svg
+    {...props}
+    xmlns='http://www.w3.org/2000/svg'
+    width={width}
+    height={width}
+    viewBox='0 0 256 256'
+  >
+    <path
+      fill='currentColor'
+      d='M172.24 99.76a6 6 0 0 1 0 8.48l-56 56a6 6 0 0 1-8.48 0l-24-24a6 6 0 0 1 8.48-8.48L112 151.51l51.76-51.75a6 6 0 0 1 8.48 0ZM230 128A102 102 0 1 1 128 26a102.12 102.12 0 0 1 102 102Zm-12 0a90 90 0 1 0-90 90a90.1 90.1 0 0 0 90-90Z'
+    />
+  </svg>
+);
