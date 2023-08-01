46e729f0c95caad3fe5c4155b8c5d05a0b1a7c84
Jungwoo <evencoding@gmail.com>
2023-08-02 01:43:03 +0900

    feat: useInputForm 기능 확장 및 골룸 생성 Form에 API 연결

--- client/src/components/goalRoomCreatePage/createGoalRoomForm/CreateGoalRoomForm.tsx
+++ client/src/components/goalRoomCreatePage/createGoalRoomForm/CreateGoalRoomForm.tsx
@@ -1,24 +1,66 @@
+import { FormEvent } from 'react';
+import { useCreateGoalRoom } from '@hooks/queries/goalRoom';
+import useValidParams from '@hooks/_common/useValidParams';
+import useFormInput from '@hooks/_common/useFormInput';
+import { CreateGoalRoomRequest } from '@myTypes/goalRoom/remote';
 import InputField from '../inputField/InputField';
 import PageSection from '../pageSection/PageSection';
-import { Stepper } from '../stepper/Stepper';
 import * as S from './CreateGoalRoomForm.styles';
 
+// 로드맵 노드 데이터
 const NODES = [
   {
+    roadmapNodeId: 1,
     title: '1. JS 기본 타입',
     content: '로드맵 1주차에는 알고리즘을 배울거에요.',
   },
   {
+    roadmapNodeId: 2,
+    title: '2. 기본 연산',
+    content: '로드맵 1주차에는 알고리즘을 배울거에요.',
+  },
+  {
+    roadmapNodeId: 3,
+    title: '2. 기본 연산',
+    content: '로드맵 1주차에는 알고리즘을 배울거에요.',
+  },
+  {
+    roadmapNodeId: 4,
     title: '2. 기본 연산',
     content: '로드맵 1주차에는 알고리즘을 배울거에요.',
   },
 ];
 
 const CreateGoalRoomForm = () => {
+  const { id: roadmapContentId } = useValidParams();
+  const { createGoalRoom } = useCreateGoalRoom();
+  const { formState, handleInputChange } = useFormInput<CreateGoalRoomRequest>({
+    roadmapContentId: Number(roadmapContentId),
+    name: '',
+    limitedMemberCount: 10,
+    goalRoomTodo: {
+      content: '',
+      startDate: '',
+      endDate: '',
+    },
+    goalRoomRoadmapNodeRequests: NODES.map(({ roadmapNodeId }) => ({
+      roadmapNodeId,
+      checkCount: 5,
+      startDate: '',
+      endDate: '',
+    })),
+  });
+
+  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    createGoalRoom(formState);
+  };
+
   return (
-    <S.Form>
+    <S.Form onSubmit={handleSubmit}>
       <InputField label='골룸명' isRequired>
-        <S.Input id='골룸명' />
+        <S.Input id='name' name='name' onChange={handleInputChange} />
       </InputField>
       <PageSection
         isRequired
@@ -27,15 +69,39 @@ const CreateGoalRoomForm = () => {
       >
         <S.NodeSectionWrapper>
           <S.NodeList nodeCount={NODES.length}>
-            {NODES.map(({ title }) => (
-              <S.NodeWrapper key={title}>
+            {NODES.map(({ roadmapNodeId, title }) => (
+              <S.NodeWrapper key={roadmapNodeId}>
                 <S.NodeInfo>{title}</S.NodeInfo>
                 <S.NodeConfigs>
-                  <InputField label='수행 시작 일자' isRequired type='small'>
-                    <S.DateInput id='수행 시작 일자' placeholder='2023-08-12' />
-                  </InputField>
+                  <>
+                    <InputField label='수행 시작 일자' isRequired type='small'>
+                      <S.DateInput
+                        id='수행 시작 일자'
+                        name={`goalRoomRoadmapNodeRequests[${
+                          roadmapNodeId - 1
+                        }][startDate]`}
+                        onChange={handleInputChange}
+                        placeholder='2023-08-12'
+                      />
+                    </InputField>
+                    <InputField label='수행 종료 일자' isRequired type='small'>
+                      <S.DateInput
+                        id='수행 종료 일자'
+                        name={`goalRoomRoadmapNodeRequests[${
+                          roadmapNodeId - 1
+                        }][endDate]`}
+                        onChange={handleInputChange}
+                        placeholder='2023-08-13'
+                      />
+                    </InputField>
+                  </>
                   <InputField label='인증 횟수' isRequired type='small'>
-                    <Stepper />
+                    <S.Input
+                      name={`goalRoomRoadmapNodeRequests[${
+                        roadmapNodeId - 1
+                      }][checkCount]`}
+                      onChange={handleInputChange}
+                    />
                   </InputField>
                 </S.NodeConfigs>
               </S.NodeWrapper>
@@ -44,8 +110,31 @@ const CreateGoalRoomForm = () => {
         </S.NodeSectionWrapper>
       </PageSection>
       <InputField label='투두리스트 생성'>
-        <S.Textarea id='투두리스트 생성' />
+        <div>
+          <InputField label='수행 시작 일자' isRequired type='small'>
+            <S.DateInput
+              id='수행 시작 일자'
+              placeholder='2023-08-12'
+              name='goalRoomTodo[startDate]'
+              onChange={handleInputChange}
+            />
+          </InputField>
+          <InputField label='수행 종료 일자' isRequired type='small'>
+            <S.DateInput
+              id='수행 종료 일자'
+              placeholder='2023-08-13'
+              name='goalRoomTodo[endDate]'
+              onChange={handleInputChange}
+            />
+          </InputField>
+        </div>
+        <S.Textarea
+          id='투두리스트 생성'
+          name='goalRoomTodo[content]'
+          onChange={handleInputChange}
+        />
       </InputField>
+      <button>생성하기</button>
     </S.Form>
   );
 };
--- client/src/hooks/_common/useFormInput.ts
+++ client/src/hooks/_common/useFormInput.ts
@@ -3,13 +3,68 @@ import { useState } from 'react';
 const useFormInput = <T extends object>(initialState: T) => {
   const [formState, setFormState] = useState<T>(initialState);
 
-  const handleInputChange = (
-    event: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>
-  ) => {
-    setFormState({
-      ...formState,
-      [event.target.name]: event.target.value,
-    });
+  const handleInputChange = ({
+    target: { name, value },
+  }: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement | HTMLSelectElement>) => {
+    // 이름을 '['를 기준으로 분리하고, ']'를 제거
+    const parts = name.split('[').map((part) => part.replace(']', ''));
+    // 배열 요소인지 확인하기 위해 첫 번째 요소가 'goalRoomRoadmapNodeRequests'인지 확인
+    const isArray = parts.length > 1 && parts[0] === 'goalRoomRoadmapNodeRequests';
+
+    // 배열 요소일 때. 즉, NodeList 내부의 값이 변했을 때
+    if (isArray) {
+      const [baseName, arrayIndex, arrayPropName] = parts;
+
+      setFormState((prevState: any) => {
+        if (Array.isArray(prevState[baseName])) {
+          return {
+            ...prevState,
+            [baseName]: prevState[baseName].map((item: any, index: number) => {
+              if (index === Number(arrayIndex)) {
+                if (arrayPropName === 'checkCount') {
+                  return {
+                    ...item,
+                    [arrayPropName]: Number(value),
+                  };
+                }
+                return {
+                  ...item,
+                  [arrayPropName]: value,
+                };
+              }
+              return item;
+            }),
+          };
+        }
+        return prevState;
+      });
+    } else {
+      // 배열 요소가 아닐 때
+      const [propName, nestedPropName] = parts;
+      setFormState((prevState: any) => {
+        // 객체 내부의 객체를 업데이트 하기 위함 (2 Depth)
+        if (nestedPropName) {
+          return {
+            ...prevState,
+            [propName]: {
+              ...prevState[propName],
+              [nestedPropName]: value,
+            },
+          };
+        }
+        // 속성을 업데이트 (1 Depth)
+        if (propName === 'limitedMemberCount') {
+          return {
+            ...prevState,
+            [propName]: Number(value),
+          };
+        }
+        return {
+          ...prevState,
+          [propName]: value,
+        };
+      });
+    }
   };
 
   const resetFormState = () => {
