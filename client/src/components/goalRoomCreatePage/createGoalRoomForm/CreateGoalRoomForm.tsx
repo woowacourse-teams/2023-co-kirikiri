be9e7ad892a0ba511f72cdefebb369015edb401d
NaveOWO <87578512+NaveOWO@users.noreply.github.com>
2023-08-02 18:06:09 +0900

    refactor: id 타입 null추가

--- client/src/components/goalRoomCreatePage/createGoalRoomForm/CreateGoalRoomForm.tsx
+++ client/src/components/goalRoomCreatePage/createGoalRoomForm/CreateGoalRoomForm.tsx
@@ -20,7 +20,7 @@ const CreateGoalRoomForm = ({ roadmapContentId, nodes }: CreateGoalRoomFormProps
     name: '',
     limitedMemberCount: 10,
     goalRoomTodo: {
-      id: -1,
+      id: null,
       content: '',
       startDate: '',
       endDate: '',
