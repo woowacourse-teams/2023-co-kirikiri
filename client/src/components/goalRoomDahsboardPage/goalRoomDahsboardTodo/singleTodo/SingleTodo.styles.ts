4b33b21ca558b7c656511338ba6dc63fa3a8969a
sh981013s <sh981013s@gmail.com>
2023-08-02 13:15:12 +0900

    refactor: shake animation 분리 후 적용

--- client/src/components/goalRoomDahsboardPage/goalRoomDahsboardTodo/singleTodo/SingleTodo.styles.ts
+++ client/src/components/goalRoomDahsboardPage/goalRoomDahsboardTodo/singleTodo/SingleTodo.styles.ts
@@ -1,22 +1,5 @@
 import styled, { keyframes } from 'styled-components';
-
-const shake = keyframes`
-  10%, 90% {
-    transform: translate3d(-0.5px, 0, 0) rotate(-0.5deg);
-  }
-
-  20%, 80% {
-    transform: translate3d(1px, 0, 0) rotate(0.5deg);
-  }
-
-  30%, 50%, 70% {
-    transform: translate3d(-0.5px, 0, 0) rotate(-0.5deg);
-  }
-
-  40%, 60% {
-    transform: translate3d(1px, 0, 0) rotate(0.5deg);
-  }
-`;
+import { shake } from '@styles/animations';
 
 export const Todo = styled.li`
   display: flex;
