1339775c02aae8b5c284c2c8798c2ca747d7e8bc
sh981013s <sh981013s@gmail.com>
2023-08-02 12:42:34 +0900

    feat: 3d box shadow theme 에 상수화 및 적용

--- client/src/components/goalRoomDahsboardPage/goalRoomCertificationFeed/GoalRoomCertificationFeed.styles.ts
+++ client/src/components/goalRoomDahsboardPage/goalRoomCertificationFeed/GoalRoomCertificationFeed.styles.ts
@@ -19,11 +19,11 @@ export const StyledImage = styled.img`
   height: 100%;
 
   object-fit: cover;
-  box-shadow: 0 1px 3px rgba(0, 0, 0, 0.12), 0 1px 2px rgba(0, 0, 0, 0.24);
+  box-shadow: ${({ theme }) => theme.shadows.threeD};
 
   transition: all 0.3s cubic-bezier(0.25, 0.8, 0.25, 1);
 
   &:hover {
-    box-shadow: 0 14px 28px rgba(0, 0, 0, 0.25), 0 10px 10px rgba(0, 0, 0, 0.22);
+    box-shadow: ${({ theme }) => theme.shadows.threeDHovered};
   }
 `;
