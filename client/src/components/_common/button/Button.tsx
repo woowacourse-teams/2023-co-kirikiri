7119ad5f68d1360cacf4b30833927e72bce66c5b
NaveOWO <87578512+NaveOWO@users.noreply.github.com>
2023-08-03 20:56:59 +0900

    feat: 버튼 공용 컴포넌트에 onclick 추가

--- client/src/components/_common/button/Button.tsx
+++ client/src/components/_common/button/Button.tsx
@@ -3,10 +3,15 @@ import * as S from './Button.styles';
 
 type ButtonProps = {
   variant?: 'primary';
+  onClick?: () => void;
 } & PropsWithChildren;
 
-const Button = ({ children, variant = 'primary' }: ButtonProps) => {
-  return <S.Button variant={variant}>{children}</S.Button>;
+const Button = ({ children, variant = 'primary', onClick }: ButtonProps) => {
+  return (
+    <S.Button variant={variant} onClick={onClick}>
+      {children}
+    </S.Button>
+  );
 };
 
 export default Button;
