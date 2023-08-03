18d9b5298bcfb07b297b6d61d8481d9efe9be8e5
Jungwoo <evencoding@gmail.com>
2023-08-03 17:22:45 +0900

    refactor: 로드맵 목록 컴포넌트 생성

---
+++ client/src/components/roadmapListPage/roadmapList/RoadmapList.tsx
@@ -0,0 +1,24 @@
+import { useRoadmapList } from '@hooks/queries/roadmap';
+import RoadmapItem from '@components/_common/roadmapItem/RoadmapItem';
+import * as S from './RoadmapList.styles';
+import { SelectedCategoryId } from '@myTypes/roadmap/internal';
+
+type RoadmapListProps = {
+  selectedCategoryId: SelectedCategoryId;
+};
+
+const RoadmapList = ({ selectedCategoryId }: RoadmapListProps) => {
+  const {
+    data: { data: roadmapList },
+  } = useRoadmapList(selectedCategoryId);
+
+  return (
+    <S.RoadmapList>
+      {roadmapList?.map((item) => (
+        <RoadmapItem key={item.roadmapId} item={item} />
+      ))}
+    </S.RoadmapList>
+  );
+};
+
+export default RoadmapList;
--- client/src/components/roadmapListPage/roadmapListView/RoadmapListView.tsx
+++ client/src/components/roadmapListPage/roadmapListView/RoadmapListView.tsx
@@ -1,15 +1,11 @@
-import RoadmapItem from '@components/_common/roadmapItem/RoadmapItem';
 import Categories from '../categories/Categories';
 
 import * as S from './RoadmapListView.styles';
-import { useRoadmapList } from '@/hooks/queries/roadmap';
 import { useSelectCategory } from '@/hooks/roadmap/useSelectCategory';
+import RoadmapList from '../roadmapList/RoadmapList';
 
 const RoadmapListView = () => {
   const [selectedCategoryId, selectCategory] = useSelectCategory();
-  const {
-    data: { data: roadmapList },
-  } = useRoadmapList(selectedCategoryId);
 
   return (
     <S.RoadmapListView>
@@ -17,11 +13,7 @@ const RoadmapListView = () => {
         selectedCategoryId={selectedCategoryId}
         selectCategory={selectCategory}
       />
-      <S.RoadmapList>
-        {roadmapList?.map((item) => (
-          <RoadmapItem key={item.roadmapId} item={item} />
-        ))}
-      </S.RoadmapList>
+      <RoadmapList selectedCategoryId={selectedCategoryId} />
     </S.RoadmapListView>
   );
 };
