46aa06e968d898a59d7d4ccf100bd531d33a9d4b
sh981013s <sh981013s@gmail.com>
2023-08-03 17:19:33 +0900

    feat: 인증 피드 전체보기 모달 구현 및 생성 로직 매핑

--- client/src/constants/goalRoom/regex.ts
+++ client/src/constants/goalRoom/regex.ts
@@ -12,3 +12,8 @@ export const TODO_END_DATE = {
   rule: /^(19|20)\d{2}-(0[1-9]|1[012])-(0[1-9]|[12][0-9]|3[01])$/,
   message: 'yyyy-mm-dd 형식으로 입력해주세요 (ex. 2021-01-01)',
 };
+
+export const CERTIFICATION_FEED = {
+  rule: /^.{1,250}$/,
+  message: '1글자부터 250글자까지 작성해주세요',
+};
