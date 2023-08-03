be35295695fee1b4ae603d5845ce57f04f0a6f06
NaveOWO <87578512+NaveOWO@users.noreply.github.com>
2023-08-03 15:06:55 +0900

    feat: form안에서 각 input들의 값을 가져오고 제출하는 커스텀 훅 생성

---
+++ client/src/hooks/roadmap/useCollectRoadmapData.ts
@@ -0,0 +1,93 @@
+import { DummyCategoryType } from '@/components/roadmapCreatePage/category/Category';
+import { DummyDifficultyType } from '@/components/roadmapCreatePage/difficulty/Difficulty';
+import { RoadmapValueType } from '@/myTypes/roadmap/remote';
+import { useEffect, useState } from 'react';
+import { useCreateRoadmap } from '../queries/roadmap';
+
+export const useCollectRoadmapData = () => {
+  const [roadmapValue, setRoadmapValue] = useState<RoadmapValueType>({
+    categoryId: null,
+    title: null,
+    introduction: null,
+    content: null,
+    difficulty: null,
+    requiredPeriod: null,
+    roadmapTags: [],
+    roadmapNodes: [],
+  });
+  const [isSumbited, setIsSubmited] = useState(false);
+  const { createRoadmap } = useCreateRoadmap();
+
+  const getSelectedCategoryId = (category: keyof DummyCategoryType | null) => {
+    setRoadmapValue((prev) => ({
+      ...prev,
+      categoryId: category,
+    }));
+  };
+
+  const getSelectedDifficulty = (difficulty: keyof DummyDifficultyType | null) => {
+    setRoadmapValue((prev) => ({
+      ...prev,
+      difficulty,
+    }));
+  };
+
+  const addNode = (e: React.MouseEvent<HTMLButtonElement>) => {
+    e.preventDefault();
+    setRoadmapValue((prev) => {
+      const tempRoadmapNodes = [...prev.roadmapNodes, {}];
+      return { ...prev, roadmapNodes: tempRoadmapNodes };
+    });
+  };
+
+  const getRoadmapItemTitle = (
+    e: React.ChangeEvent<HTMLInputElement>,
+    itemId: number
+  ) => {
+    setRoadmapValue((prev) => {
+      const inputName = e.target.name;
+      const tempRoadmapNodes = [...prev.roadmapNodes];
+      tempRoadmapNodes[itemId] = { ...tempRoadmapNodes[itemId] };
+      tempRoadmapNodes[itemId][inputName] = e.target.value;
+      return { ...prev, roadmapNodes: tempRoadmapNodes };
+    });
+  };
+
+  const getTags = (tags: string[]) => {
+    setRoadmapValue((prev) => {
+      const newTags = tags.map((tag) => {
+        return { name: tag };
+      });
+      return { ...prev, roadmapTags: [...newTags] };
+    });
+  };
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const target = e.target as HTMLFormElement;
+    setRoadmapValue((prev) => ({
+      ...prev,
+      title: target.roadmapTitle.value,
+      introduction: target.introduction.value,
+      content: target.content.value,
+      requiredPeriod: target.requiredPeriod.value,
+    }));
+    setIsSubmited(true);
+  };
+
+  useEffect(() => {
+    if (isSumbited) {
+      createRoadmap(roadmapValue);
+    }
+  }, [isSumbited]);
+
+  return {
+    roadmapValue,
+    getSelectedCategoryId,
+    getSelectedDifficulty,
+    getRoadmapItemTitle,
+    getTags,
+    handleSubmit,
+    addNode,
+  };
+};
