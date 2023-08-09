dacedbaca8efbeac36d1c9167022bb40637f6510
NaveOWO <87578512+NaveOWO@users.noreply.github.com>
2023-08-09 15:37:22 +0900

    [feat/CK-142] 로드맵 생성페이지 버그를 해결한다 (#101)
    * design: 태그 삭제버튼 생성
    
    * feat: 태그 삭제버튼 생성 & 태그 placeholder적용
    
    * feat: 태그를 삭제하는 로직 구현
    
    * style: 코드 스타일 정리
    
    * refactor: 태그를 완성하는 trigger 분리 후 적용
    
    * fix: 로드맵 입력부분 크기가 맞지 않는 오류 해결
    
    * fix: 소개글 입력창의 높이가 늘어나지 않는 오류 해결
    
    * refactor: 로드맵 입력 placeholder추가
    
    * style: 오타 수정

--- client/src/components/roadmapCreatePage/input/inputField/inputField.styles.ts
+++ client/src/components/roadmapCreatePage/input/inputField/inputField.styles.ts
@@ -4,6 +4,7 @@ export const Input = styled.textarea`
   width: 80%;
   height: 80%;
   color: ${({ theme }) => theme.colors.gray300};
+
   &::placeholder {
     ${({ theme }) => theme.fonts.description4};
     color: ${({ theme }) => theme.colors.gray200};
