356b57e3d484fe88a688c0063ac092e5d160b710
ChoiYoungHoon <dudgns2865@naver.com>
2023-08-10 17:46:33 +0900

    feat: UUID 기반으로 file path 제공 구현

--- backend/kirikiri/src/main/java/co/kirikiri/service/UUIDFilePathGenerator.java
+++ backend/kirikiri/src/main/java/co/kirikiri/service/UUIDFilePathGenerator.java
@@ -6,18 +6,19 @@ import java.time.format.DateTimeFormatter;
 import java.util.UUID;
 
 @Component
-public class FilePathRandomGenerator implements FilePathGenerator {
+public class UUIDFilePathGenerator implements FilePathGenerator {
 
     private static final String DIRECTORY_SEPARATOR = "/";
+    private static final String UUID_ORIGINAL_FILE_NAME_SEPARATOR = "_";
 
     @Override
-    public String makeFilePath(final Long id, final ImageDirType dirType) {
+    public String makeFilePath(final ImageDirType dirType, final String originalFileName) {
         final LocalDate currentDate = LocalDate.now();
         final DateTimeFormatter formatter = DateTimeFormatter.ofPattern(
                 String.format("yyyy%sMMdd", DIRECTORY_SEPARATOR));
         final String dateString = currentDate.format(formatter);
 
-        return dateString + DIRECTORY_SEPARATOR + dirType.getDirName() + DIRECTORY_SEPARATOR + id
-                + DIRECTORY_SEPARATOR + UUID.randomUUID() + "_";
+        return DIRECTORY_SEPARATOR + dateString + DIRECTORY_SEPARATOR + dirType.getDirName()
+                + DIRECTORY_SEPARATOR + UUID.randomUUID() + UUID_ORIGINAL_FILE_NAME_SEPARATOR + originalFileName;
     }
 }
