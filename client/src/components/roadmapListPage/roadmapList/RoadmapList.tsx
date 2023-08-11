0a7ac5233c4fa3e768b620fda96a801ad47e5083
sh981013s <sh981013s@gmail.com>
2023-08-11 17:09:02 +0900

    feat: 무한 스크롤 적용

--- client/src/components/roadmapListPage/roadmapList/RoadmapList.tsx
+++ client/src/components/roadmapListPage/roadmapList/RoadmapList.tsx
@@ -3,13 +3,21 @@ import RoadmapItem from '@components/_common/roadmapItem/RoadmapItem';
 import * as S from './RoadmapList.styles';
 import { SelectedCategoryId } from '@myTypes/roadmap/internal';
 import { useNavigate } from 'react-router-dom';
+import { useInfiniteScroll } from '@hooks/queries/useInfiniteScroll';
 
 type RoadmapListProps = {
   selectedCategoryId: SelectedCategoryId;
 };
 
 const RoadmapList = ({ selectedCategoryId }: RoadmapListProps) => {
-  const { data: roadmapList } = useRoadmapList({ categoryId: selectedCategoryId });
+  const { roadmapListResponse, fetchNextPage } = useRoadmapList({
+    categoryId: selectedCategoryId,
+  });
+  const loadMoreRef = useInfiniteScroll({
+    hasNextPage: roadmapListResponse.hasNext,
+    fetchNextPage,
+  });
+
   const navigate = useNavigate();
 
   const moveRoadmapCreatePage = () => {
@@ -18,10 +26,10 @@ const RoadmapList = ({ selectedCategoryId }: RoadmapListProps) => {
 
   return (
     <S.RoadmapList aria-label='로드맵 목록'>
-      {roadmapList &&
-        roadmapList.responses.map((item) => (
-          <RoadmapItem key={item.roadmapId} item={item} roadmapId={item.roadmapId} />
-        ))}
+      {roadmapListResponse.responses.map((item) => (
+        <RoadmapItem key={item.roadmapId} item={item} roadmapId={item.roadmapId} />
+      ))}
+      <div ref={loadMoreRef}>Loading more roadmaps...</div>
       <S.CreateRoadmapButton onClick={moveRoadmapCreatePage}>
         로드맵 생성하러가기
       </S.CreateRoadmapButton>
