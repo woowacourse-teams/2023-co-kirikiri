e6d3020119a92c7e3a445281680f3f85d1be8993
NaveOWO <87578512+NaveOWO@users.noreply.github.com>
2023-08-13 18:13:10 +0900

    feat: 무한스크롤 적용된 api명세 반영

--- client/src/hooks/queries/goalRoom.ts
+++ client/src/hooks/queries/goalRoom.ts
@@ -27,7 +27,7 @@ export const useGoalRoomList = (params: GoalRoomListRequest) => {
   const { data } = useSuspendedQuery(['goalRoomList', params.roadmapId], () =>
     getGoalRoomList(params)
   );
-  return { goalRoomList: data };
+  return { goalRoomList: data.responses };
 };
 
 export const useMyPageGoalRoomList = (statusCond: GoalRoomRecruitmentStatus) => {
