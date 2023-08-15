6db63ff54d59ba4eabedfd045ae24bccaa0025b2
NaveOWO <87578512+NaveOWO@users.noreply.github.com>
2023-08-15 16:45:28 +0900

    feat: 로드맵 검색 api 로직 생성

--- client/src/apis/roadmap.ts
+++ client/src/apis/roadmap.ts
@@ -25,6 +25,22 @@ export const getRoadmapList = async (
   return data;
 };
 
+// 부엉이와 코드 충돌 방지하기 위해 request & response 타입은 any를 써두겠습니다 :)
+export const getSearchRoadmapList = async ({
+  roadmapTitle,
+  creatorId,
+  tagName,
+  filterCond = 'LATEST',
+  lastId,
+  size = 10,
+}: any) => {
+  const { data } = await client.get<any>(
+    `/api/roadmaps/search?roadmapTitle=${roadmapTitle}&lastId=${lastId}&creatorId=${creatorId}&tagName=${tagName}&filterCond=${filterCond}&size=${size}`
+  );
+
+  return data;
+};
+
 export const getRoadmapDetail = async (id: number): Promise<RoadmapDetailResponse> => {
   const { data } = await client.get<RoadmapDetailResponse>(`/roadmaps/${id}`);
 
