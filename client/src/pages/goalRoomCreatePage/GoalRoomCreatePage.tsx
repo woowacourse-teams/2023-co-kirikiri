1ae421d97311bd7991a8cb6837a08642ebac4c38
Jungwoo <evencoding@gmail.com>
2023-08-15 21:33:43 +0900

    refactor: 골룸 생성 페이지에 Suspense 적용

--- client/src/pages/goalRoomCreatePage/GoalRoomCreatePage.tsx
+++ client/src/pages/goalRoomCreatePage/GoalRoomCreatePage.tsx
@@ -1,11 +1,15 @@
+import { Suspense } from 'react';
 import CreateGoalRoom from '@components/goalRoomCreatePage/createGoalRoom/CreateGoalRoom';
+import Spinner from '@components/_common/spinner/Spinner';
 import * as S from './GoalRoomCreatePage.styles';
 
 const GoalRoomCreatePage = () => {
   return (
     <S.GoalRoomCreatePage>
       <S.PageTitle>골룸을 생성해주세요!</S.PageTitle>
-      <CreateGoalRoom />
+      <Suspense fallback={<Spinner />}>
+        <CreateGoalRoom />
+      </Suspense>
     </S.GoalRoomCreatePage>
   );
 };
