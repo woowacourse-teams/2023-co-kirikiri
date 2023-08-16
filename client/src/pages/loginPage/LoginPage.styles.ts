0d52e4dc9b76920230f73b305bcd82f084936f35
sh981013s <sh981013s@gmail.com>
2023-08-17 03:54:44 +0900

    design: 코끼리 로고 중앙정렬

--- client/src/pages/loginPage/LoginPage.styles.ts
+++ client/src/pages/loginPage/LoginPage.styles.ts
@@ -1,8 +1,37 @@
 import styled from 'styled-components';
 
 export const LoginPageWrapper = styled.div`
+  position: relative;
+
   display: flex;
   align-items: center;
   justify-content: center;
+
   height: 100vh;
+
+  @keyframes float {
+    0% {
+      transform: translateY(0);
+    }
+    50% {
+      transform: translateY(-20px);
+    }
+    100% {
+      transform: translateY(0);
+    }
+  }
+`;
+
+export const ElephantImage = styled.img`
+  position: absolute;
+  top: 10%;
+  right: 10%;
+
+  width: 200px;
+  height: 200px;
+
+  background-repeat: no-repeat;
+  background-size: contain;
+
+  animation: float 5s ease-in-out infinite;
 `;
