4bb3c2f87e6a617c0aad0fb52c026400a915d0ec
NaveOWO <87578512+NaveOWO@users.noreply.github.com>
2023-08-17 14:00:29 +0900

    feat: 불피요한 컴포넌트 제거

--- client/src/pages/roadmapCreatePage/RoadmapCreatePage.tsx
+++ client/src/pages/roadmapCreatePage/RoadmapCreatePage.tsx
@@ -1,11 +1,9 @@
 import RoadmapCreateForm from '@/components/roadmapCreatePage/roadmapCreateForm/RoadmapCreateForm';
-import RoadmapSearch from '@/components/roadmapListPage/roadmapSearch/RoadmapSearch';
 
 const RoadmapCreatePage = () => {
   return (
     <>
       <RoadmapCreateForm />
-      <RoadmapSearch />
     </>
   );
 };
