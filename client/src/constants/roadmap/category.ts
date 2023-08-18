71b5d3915d088e681f7e81d5c71dd7369641d71c
NaveOWO <87578512+NaveOWO@users.noreply.github.com>
2023-08-18 16:32:27 +0900

    fix: 없는 카테고리 삭제 (#137)

--- client/src/constants/roadmap/category.ts
+++ client/src/constants/roadmap/category.ts
@@ -1,51 +1,51 @@
 export const CategoriesInfo = {
+  // 0: {
+  //   id: 0,
+  //   name: '전체',
+  //   iconName: 'AllCategoryIcon',
+  // },
   1: {
     id: 1,
-    name: '전체',
-    iconName: 'AllCategoryIcon',
-  },
-  2: {
-    id: 2,
     name: '어학',
     iconName: 'LanguageIcon',
   },
-  3: {
-    id: 3,
+  2: {
+    id: 2,
     name: 'IT',
     iconName: 'ITIcon',
   },
-  4: {
-    id: 4,
+  3: {
+    id: 3,
     name: '시험',
     iconName: 'ExamIcon',
   },
-  5: {
-    id: 5,
+  4: {
+    id: 4,
     name: '운동',
     iconName: 'ExerciseIcon',
   },
-  6: {
-    id: 6,
+  5: {
+    id: 5,
     name: '게임',
     iconName: 'GameIcon',
   },
-  7: {
-    id: 7,
+  6: {
+    id: 6,
     name: '음악',
     iconName: 'MusicIcon',
   },
-  8: {
-    id: 8,
+  7: {
+    id: 7,
     name: '라이프',
     iconName: 'LifeIcon',
   },
-  9: {
-    id: 9,
+  8: {
+    id: 8,
     name: '여가',
     iconName: 'HobbyIcon',
   },
-  10: {
-    id: 10,
+  9: {
+    id: 9,
     name: '기타',
     iconName: 'EtcIcon',
   },
