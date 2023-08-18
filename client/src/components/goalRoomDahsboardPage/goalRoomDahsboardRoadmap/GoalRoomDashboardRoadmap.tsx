2593276d86b1a42e803aad87bd35787fbb1e0260
NaveOWO <87578512+NaveOWO@users.noreply.github.com>
2023-08-18 12:45:02 +0900

    feat: 골룸 대시보드 로드맵 구현

--- client/src/components/goalRoomDahsboardPage/goalRoomDahsboardRoadmap/GoalRoomDashboardRoadmap.tsx
+++ client/src/components/goalRoomDahsboardPage/goalRoomDahsboardRoadmap/GoalRoomDashboardRoadmap.tsx
@@ -1,17 +1,30 @@
 import * as S from './GoalRoomDashboardRoadmap.styles';
-import SVGIcon from '@components/icons/SVGIcon';
+// import SVGIcon from '@components/icons/SVGIcon';
+import { useGoalRoomDetail } from '@/hooks/queries/goalRoom';
+import { useGoalRoomDashboardContext } from '@/context/goalRoomDashboardContext';
 
 const GoalRoomDashboardRoadmap = () => {
+  const { goalroomId } = useGoalRoomDashboardContext();
+  const { goalRoomInfo } = useGoalRoomDetail(Number(goalroomId));
+
   return (
     <S.RoadmapWrapper>
       <div>
         <h2>로드맵</h2>
-        <button>
-          <span>전체보기</span>
-          <SVGIcon name='RightArrowIcon' />
-        </button>
       </div>
-      <div>로드맵</div>
+      <S.RoadmapContainer>
+        {goalRoomInfo.goalRoomNodes.map((node) => {
+          return (
+            <S.NodeContainer>
+              <S.NodePeriod>
+                {node.startDate} ~ {node.endDate}
+              </S.NodePeriod>
+              <S.NodeTitle>{node.title}</S.NodeTitle>
+              <S.FeedCount>인증횟수 {node.checkCount}회</S.FeedCount>
+            </S.NodeContainer>
+          );
+        })}
+      </S.RoadmapContainer>
     </S.RoadmapWrapper>
   );
 };
