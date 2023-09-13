4ac964c13ba72de63addea31ce465a4cb2823fdd
NaveOWO <87578512+NaveOWO@users.noreply.github.com>
2023-09-13 15:10:35 +0900

    feat: error-boundary를 응집한 AsyncBoundary 생성

--- client/src/components/_common/errorBoundary/AsyncBoundary.tsx
+++ client/src/components/_common/errorBoundary/AsyncBoundary.tsx
@@ -1,5 +1,4 @@
 import { PropsWithChildren, Suspense } from 'react';
-import { Critical, NotFound, Runtime, ServerError } from '../error/ErrorComponents';
 import Spinner from '../spinner/Spinner';
 import CriticalErrorBoundary from './CriticalErrorBoundary';
 import NotFoundErrorBoundary from './NotFoundErrorBoundary';
@@ -8,10 +7,10 @@ import ServerErrorBoundary from './ServerErrorBoundary';
 
 const AsyncBoundary = ({ children }: PropsWithChildren) => {
   return (
-    <CriticalErrorBoundary fallbackRender={() => <Critical />}>
-      <RuntimeErrorBoundary fallbackRender={() => <Runtime />}>
-        <ServerErrorBoundary fallbackRender={() => <ServerError />}>
-          <NotFoundErrorBoundary fallbackRender={() => <NotFound />}>
+    <CriticalErrorBoundary>
+      <RuntimeErrorBoundary>
+        <ServerErrorBoundary>
+          <NotFoundErrorBoundary>
             <Suspense fallback={<Spinner />}>{children}</Suspense>
           </NotFoundErrorBoundary>
         </ServerErrorBoundary>
