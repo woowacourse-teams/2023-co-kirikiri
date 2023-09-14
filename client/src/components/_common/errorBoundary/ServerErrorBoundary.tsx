9e3f44d9f64715bd7ba5f43c3c0ac778973a2714
NaveOWO <87578512+NaveOWO@users.noreply.github.com>
2023-09-14 15:27:50 +0900

    feat: 에러 코드 확인하는 정규표현식 분리

--- client/src/components/_common/errorBoundary/ServerErrorBoundary.tsx
+++ client/src/components/_common/errorBoundary/ServerErrorBoundary.tsx
@@ -1,11 +1,11 @@
+import { SERVER_ERROR_CODE } from '@/constants/_common/regex';
 import { ErrorInfo } from 'react';
 import { ServerError } from '../error/ErrorComponents';
 import ErrorBoundary from './ErrorBoundary';
 
 class ServerErrorBoundary extends ErrorBoundary {
   componentDidCatch(error: any, _errorInfo: ErrorInfo): void {
-    const status = /5\d{2}/;
-    if (!status.test(error.response.status)) throw error;
+    if (!SERVER_ERROR_CODE.test(error.response.status)) throw error;
   }
 
   render() {
---
+++ client/src/constants/_common/regex.ts
@@ -0,0 +1,1 @@
+export const SERVER_ERROR_CODE = /5\d{2}/;
