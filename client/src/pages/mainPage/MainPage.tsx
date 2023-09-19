a2a753cb8d68492a24e8cec9e285b6fc96b7203e
NaveOWO <87578512+NaveOWO@users.noreply.github.com>
2023-09-19 13:23:40 +0900

    design: 랜딩페이지 디자인 수정사항 반영

--- client/src/pages/mainPage/MainPage.tsx
+++ client/src/pages/mainPage/MainPage.tsx
@@ -39,17 +39,6 @@ const MainPage = () => {
           우리들이 함께 만들어가는 로드맵 기반 스터디 플랫폼.
         </S.MainPageDesc>
         <S.MainPageTitle>코끼리끼리</S.MainPageTitle>
-        <S.ServiceDescWrapper>
-          <S.ServiceDescContent>
-            <S.ServiceDesc>
-              코끼리끼리에서 나만의 로드맵을 자유롭게 제공해보세요! <br /> 전문적인 지식
-              공유도 좋고, 가벼운 취미 공유도 좋습니다. <br /> 매력적인 로드맵은
-              여러사람들을 움직이게 할 거 에요! <br /> 로드맵을 달성하길 원하는 사람들은
-              '목표를 달성하는 방'이라는 뜻의 모임을 생성해서 모일 수 있어요. <br />{' '}
-              코끼리끼리를 통해 함께 목표를 달성해봐요!
-            </S.ServiceDesc>
-          </S.ServiceDescContent>
-        </S.ServiceDescWrapper>
         <S.GoalRoomListButton onClick={listButtonHandler}>
           지금 바로 로드맵 확인하러 가기
         </S.GoalRoomListButton>
