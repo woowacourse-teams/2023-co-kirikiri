7a93efa9a4ccb96c65c090c8a69d9c327e937ab9
NaveOWO <87578512+NaveOWO@users.noreply.github.com>
2023-09-19 15:24:02 +0900

    feat: 골룸 갱신 queryKey 추가

--- client/src/hooks/queries/goalRoom.ts
+++ client/src/hooks/queries/goalRoom.ts
@@ -85,7 +85,11 @@ export const useCreateGoalRoom = (roadmapContentId: number) => {
     (body: CreateGoalRoomRequest) => postCreateGoalRoom(body),
     {
       async onSuccess() {
-        await queryClient.refetchQueries([QUERY_KEYS.goalRoom.list, roadmapContentId]);
+        await queryClient.refetchQueries([
+          QUERY_KEYS.goalRoom.list,
+          QUERY_KEYS.goalRoom.my,
+          roadmapContentId,
+        ]);
         navigate(`/roadmap/${roadmapContentId}/goalroom-list`);
         triggerToast({ message: '모임을 생성했습니다!' });
       },
