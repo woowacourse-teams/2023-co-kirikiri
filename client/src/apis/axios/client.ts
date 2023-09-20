3d3d321d363c65e91488041f9f29ec9ef0179be0
sh981013s <sh981013s@gmail.com>
2023-09-20 17:20:38 +0900

    feat: token reissue 에러 핸들링 및 session Handler wrapper component 구현

--- client/src/apis/axios/client.ts
+++ client/src/apis/axios/client.ts
@@ -23,52 +23,57 @@ client.interceptors.request.use((config) => {
   return config;
 });
 
+let globalRetryCount = 0;
+
 client.interceptors.response.use(
-  (response) => response,
+  (response) => {
+    return response;
+  },
   async (error) => {
     const originalRequest = error.config;
 
-    if (!originalRequest.retryCount) {
-      originalRequest.retryCount = 0;
+    if (globalRetryCount >= 3) {
+      window.dispatchEvent(new Event('unauthorized'));
+      return Promise.reject(error);
     }
 
-    if (error.response) {
-      if (
-        error.response.status === 401 &&
-        error.response.data.message === 'Expired Token'
-      ) {
-        if (originalRequest.retryCount >= 3) {
-          window.location.href = '/login';
-          return;
+    if (
+      error.response &&
+      error.response.status === 401 &&
+      error.response.data.message === 'Expired Token'
+    ) {
+      globalRetryCount++;
+
+      const refreshToken = getCookie('refresh_token');
+
+      try {
+        const { data } = await client.post('/auth/reissue', { refreshToken });
+
+        setCookie('access_token', data.accessToken);
+        setCookie('refresh_token', data.refreshToken);
+
+        return client(originalRequest);
+      } catch (reissueError) {
+        const axiosError = reissueError as AxiosError<ErrorData>;
+
+        if (axiosError.response?.status === 401) {
+          window.dispatchEvent(new Event('unauthorized'));
+          return Promise.reject(reissueError);
+        } else {
+          return Promise.reject(reissueError);
         }
-        originalRequest.retryCount++;
-
-        const refreshToken = getCookie('refresh_token');
-
-        try {
-          const { data } = await client.post('/auth/reissue', { refreshToken });
-
-          setCookie('access_token', data.accessToken);
-          setCookie('refresh_token', data.refreshToken);
-
-          return client(originalRequest);
-        } catch (reissueError) {
-          const axiosError = reissueError as AxiosError<ErrorData>;
-
-          if (
-            axiosError.response?.status === 401 &&
-            axiosError.response?.data?.message === '토큰이 유효하지 않습니다.'
-          ) {
-            return client(originalRequest);
-          } else {
-            throw reissueError;
-          }
-        }
-      } else {
-        throw error;
       }
+    } else if (
+      error.response &&
+      error.response.status === 501 &&
+      error.response.data.message.includes(
+        'Row was updated or deleted by another transaction'
+      )
+    ) {
+      return null;
+    } else {
+      return Promise.reject(error);
     }
-    throw error;
   }
 );
 
