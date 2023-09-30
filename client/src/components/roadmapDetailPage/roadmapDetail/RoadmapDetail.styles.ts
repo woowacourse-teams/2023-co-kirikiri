efe0c38d7169a70d7eb6482c48549a3f8f10bf78
Jungwoo <evencoding@gmail.com>
2023-09-30 17:30:31 +0900

    feat: 로드맵 디테일 컴포넌트 UI 개편

--- client/src/components/roadmapDetailPage/roadmapDetail/RoadmapDetail.styles.ts
+++ client/src/components/roadmapDetailPage/roadmapDetail/RoadmapDetail.styles.ts
@@ -1,42 +1,19 @@
-import media from '@styles/media';
 import styled from 'styled-components';
 
 export const RoadmapDetail = styled.div`
-  position: relative;
-  margin: 4rem 0;
-  padding: 0 2rem;
-
-  ${media.mobile`
-    flex-direction: column;
-    align-items: center;
-  `}
+  padding: 2rem 0 4rem 0;
 `;
 
-export const RoadmapBody = styled.p`
-  ${({ theme }) => theme.fonts.button1}
-  width: 50%;
-  padding: 4rem 4rem;
-  height: 35rem;
-
-  overflow: scroll;
-
-  border-radius: 18px;
-  box-shadow: ${({ theme }) => theme.shadows.box};
-
-  color: ${({ theme }) => theme.colors.gray300};
-
-  ${media.mobile`
-    padding: 4rem 4rem;
-  `}
-
-  & > strong {
-    ${({ theme }) => theme.fonts.h1};
-    margin-bottom: 4rem;
-    color: ${({ theme }) => theme.colors.main_dark};
-  }
+export const RoadmapInfo = styled.div`
+  margin-bottom: 4rem;
 `;
 
-export const PageOnTop = styled.div`
+export const Title = styled.div`
+  ${({ theme }) => theme.fonts.title_large};
+  margin-bottom: 2rem;
+  color: ${({ theme }) => theme.colors.main_dark};
+`;
+
+export const Description = styled.div`
   display: flex;
-  justify-content: space-around;
 `;
