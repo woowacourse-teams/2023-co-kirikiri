e01e873566b684429c242fd2be1cfa8adb86299a
Jungwoo <evencoding@gmail.com>
2023-09-30 17:29:49 +0900

    feat: NodeContent(각 노드 정보 Slider) 컴포넌트 구현

---
+++ client/src/components/roadmapDetailPage/nodeContent/NodeContent.styles.ts
@@ -0,0 +1,92 @@
+import styled from 'styled-components';
+import media from '@styles/media';
+
+export const SliderContent = styled.div`
+  display: flex;
+  aspect-ratio: 5 / 3.5;
+  background-color: ${({ theme }) => theme.colors.gray100};
+  border-radius: 8px;
+
+  ${media.mobile`
+    flex-direction: column;
+    aspect-ratio: 0;
+  `}
+`;
+
+export const LeftContent = styled.div`
+  flex-shrink: 0;
+  width: 45%;
+
+  ${media.mobile`
+    display:none;
+  `}
+`;
+
+export const NodeImg = styled.img`
+  width: 100%;
+  height: 100%;
+  padding: 1.5rem;
+  object-fit: cover;
+`;
+
+export const NoImg = styled.div`
+  ${({ theme }) => theme.fonts.title_large}
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+
+  width: 100%;
+  height: 100%;
+`;
+
+export const Separator = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 0.2rem;
+  height: 100%;
+
+  & > div {
+    height: 50%;
+  }
+
+  & > div:last-child {
+    background-color: black;
+  }
+`;
+
+export const RightContent = styled.div`
+  ${({ theme }) => theme.fonts.h1}
+  overflow: scroll;
+  width: 55%;
+  padding: 1.5rem;
+  padding-top: 3rem;
+
+  ${media.mobile`
+    width: 100%;
+    height: 60rem;
+    padding-top: 1.5rem;
+  `}
+`;
+
+export const ContentTitle = styled.div`
+  ${({ theme }) => theme.fonts.title_large}
+  display: flex;
+  align-items: center;
+  margin-bottom: 1rem;
+`;
+
+export const Step = styled.div`
+  ${({ theme }) => theme.fonts.h2}
+  display: flex;
+  flex-shrink: 0;
+  align-items: center;
+  justify-content: center;
+
+  width: 3rem;
+  height: 3rem;
+  margin-right: 0.5rem;
+
+  border: 0.3rem solid black;
+  border-radius: 50%;
+`;
