a61e8a43cd21d37125980b476705332c17ab8ed4
NaveOWO <87578512+NaveOWO@users.noreply.github.com>
2023-11-01 14:48:07 +0900

    feat: Network에러바운더리 생성

---
+++ client/src/components/_common/errorBoundary/NetworkErrorBoundary.tsx
@@ -0,0 +1,28 @@
+import { ErrorInfo } from 'react';
+import ErrorBoundary from './ErrorBoundary';
+import { NetworkError } from './errors';
+
+export class NetworkErrorBoundary extends ErrorBoundary {
+  static getDerivedStateFromError(error: Error): { didCatch: boolean; error: Error } {
+    if (error instanceof NetworkError) {
+      return { didCatch: true, error };
+    }
+    throw error;
+  }
+
+  componentDidCatch(error: Error, errorInfo: ErrorInfo): void {
+    if (this.props.isCritical) throw error;
+
+    this.props.onError?.(error, errorInfo);
+    this.setState({ fallback: <div>network error</div> });
+  }
+
+  render() {
+    const { didCatch, fallback: innerFallback } = this.state;
+    const { children, fallback: customFllback } = this.props;
+    if (didCatch) {
+      return customFllback ?? innerFallback ?? <div>network error!</div>;
+    }
+    return children;
+  }
+}
