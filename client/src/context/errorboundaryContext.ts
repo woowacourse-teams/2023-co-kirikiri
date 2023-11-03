69bc625d301412792991f837b79acdf772ce40b5
NaveOWO <87578512+NaveOWO@users.noreply.github.com>
2023-11-03 22:35:07 +0900

    chore: 사용하지 않는 파일 삭제

--- client/src/context/errorboundaryContext.ts
+++
@@ -1,4 +0,0 @@
-import { ErrorBoundaryContextType } from '@/myTypes/_common/errorBoundary';
-import { createContext } from 'react';
-
-export const ErrorBoundaryContext = createContext<ErrorBoundaryContextType | null>(null);
