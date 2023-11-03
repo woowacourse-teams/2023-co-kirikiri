7206e625e4401cb102afe7e3a3e26142478d494e
NaveOWO <87578512+NaveOWO@users.noreply.github.com>
2023-11-03 22:35:47 +0900

    design: 토스트 아이콘 삽입

--- client/src/components/icons/svgIcons.tsx
+++ client/src/components/icons/svgIcons.tsx
@@ -2503,3 +2503,48 @@ export const NoImageIcon = ({ width, ...props }: SVGProps<SVGSVGElement>) => (
     />
   </svg>
 );
+
+export const SuccessIcon = ({ width, ...props }: SVGProps<SVGSVGElement>) => (
+  <svg
+    {...props}
+    xmlns='http://www.w3.org/2000/svg'
+    height='24'
+    viewBox='0 -960 960 960'
+    width='24'
+  >
+    <path
+      fill='white'
+      d='m424-296 282-282-56-56-226 226-114-114-56 56 170 170Zm56 216q-83 0-156-31.5T197-197q-54-54-85.5-127T80-480q0-83 31.5-156T197-763q54-54 127-85.5T480-880q83 0 156 31.5T763-763q54 54 85.5 127T880-480q0 83-31.5 156T763-197q-54 54-127 85.5T480-80Zm0-80q134 0 227-93t93-227q0-134-93-227t-227-93q-134 0-227 93t-93 227q0 134 93 227t227 93Zm0-320Z'
+    />
+  </svg>
+);
+
+export const ErrorIcon = ({ width, ...props }: SVGProps<SVGSVGElement>) => (
+  <svg
+    {...props}
+    xmlns='http://www.w3.org/2000/svg'
+    height='24'
+    viewBox='0 -960 960 960'
+    width='24'
+  >
+    <path
+      fill='white'
+      d='M480-280q17 0 28.5-11.5T520-320q0-17-11.5-28.5T480-360q-17 0-28.5 11.5T440-320q0 17 11.5 28.5T480-280Zm-40-160h80v-240h-80v240Zm40 360q-83 0-156-31.5T197-197q-54-54-85.5-127T80-480q0-83 31.5-156T197-763q54-54 127-85.5T480-880q83 0 156 31.5T763-763q54 54 85.5 127T880-480q0 83-31.5 156T763-197q-54 54-127 85.5T480-80Zm0-80q134 0 227-93t93-227q0-134-93-227t-227-93q-134 0-227 93t-93 227q0 134 93 227t227 93Zm0-320Z'
+    />
+  </svg>
+);
+
+export const WarningIcon = ({ width, ...props }: SVGProps<SVGSVGElement>) => (
+  <svg
+    {...props}
+    xmlns='http://www.w3.org/2000/svg'
+    height='24'
+    viewBox='0 -960 960 960'
+    width='24'
+  >
+    <path
+      fill='white'
+      d='m40-120 440-760 440 760H40Zm138-80h604L480-720 178-200Zm302-40q17 0 28.5-11.5T520-280q0-17-11.5-28.5T480-320q-17 0-28.5 11.5T440-280q0 17 11.5 28.5T480-240Zm-40-120h80v-200h-80v200Zm40-100Z'
+    />
+  </svg>
+);
