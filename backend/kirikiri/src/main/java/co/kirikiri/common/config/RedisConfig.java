f609e230c27d127429da9d9d5e4f910001f27bf9
ChoiYoungHoon <dudgns2865@naver.com>
2023-11-09 00:45:46 +0900

    fix: RedisConfig를 test에서 비활성화 시킨다

--- backend/kirikiri/src/main/java/co/kirikiri/common/config/RedisConfig.java
+++ backend/kirikiri/src/main/java/co/kirikiri/common/config/RedisConfig.java
@@ -9,6 +9,7 @@ import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.data.redis.cache.RedisCacheConfiguration;
 import org.springframework.data.redis.cache.RedisCacheManager;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
@@ -20,8 +21,9 @@ import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
 @EnableCaching
+@Profile(value = {"prod", "dev", "local"})
 public class RedisConfig {
-    
+
     @Bean
     public RedisTemplate<String, String> redisTemplate(final RedisConnectionFactory redisConnectionFactory) {
         final RedisTemplate<String, String> redisTemplate = new RedisTemplate<>();
