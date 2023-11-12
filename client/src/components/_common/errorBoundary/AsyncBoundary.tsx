7c74cea9af52400dc77db44e1ab5272046c4a394
NaveOWO <87578512+NaveOWO@users.noreply.github.com>
2023-11-12 17:03:23 +0900

    feat: 에러바운더리에 callback props추가

--- client/src/components/_common/errorBoundary/AsyncBoundary.tsx
+++ client/src/components/_common/errorBoundary/AsyncBoundary.tsx
@@ -1,3 +1,4 @@
+import { ErrorBoundarySharedProps } from '@/myTypes/_common/errorBoundary';
 import { PropsWithChildren, Suspense } from 'react';
 import Spinner from '../spinner/Spinner';
 import { ApiErrorBoundary } from './ApiErrorBoundary';
@@ -8,17 +9,28 @@ import RuntimeErrorBoundary from './RuntimeErrorBoundary';
 
 interface AsyncBoundaryProps {
   isCritical?: boolean;
+  onError?: ErrorBoundarySharedProps['onError'];
+  onReset?: ErrorBoundarySharedProps['onReset'];
+  resetKeys?: unknown[];
 }
 
 const AsyncBoundary = ({
   children,
   isCritical,
+  onError,
+  onReset,
+  resetKeys,
 }: PropsWithChildren<AsyncBoundaryProps>) => {
   return (
     <RuntimeErrorBoundary>
       <ApiErrorBoundary<APIError>>
         <NetworkErrorBoundary>
-          <BaseErrorBoundary isCritical={isCritical}>
+          <BaseErrorBoundary
+            isCritical={isCritical}
+            onError={onError}
+            onReset={onReset}
+            resetKeys={resetKeys}
+          >
             <Suspense fallback={<Spinner />}>{children}</Suspense>
           </BaseErrorBoundary>
         </NetworkErrorBoundary>
