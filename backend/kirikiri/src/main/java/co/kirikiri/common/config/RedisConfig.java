b9e68414afdd8cba4e86541961446d525d8d2935
YoungHoon Choi <90854702+younghoondoodoom@users.noreply.github.com>
2023-11-13 13:59:28 +0900

    [feat/CK-235] Redis 캐시를 적용한다 (#197)
    * test: test container에 의존하지 않도록 변경
    
    * feat: redis cache 적용
    
    * feat: redis config 설정 수정
    
    * fix: RedisConfig를 test에서 비활성화 시킨다
    
    * test: CacheKeyGenerator test code 작성
    
    * feat: 수정 요구사항 반영

--- backend/kirikiri/src/main/java/co/kirikiri/common/config/RedisConfig.java
+++ backend/kirikiri/src/main/java/co/kirikiri/common/config/RedisConfig.java
@@ -1,33 +1,52 @@
 package co.kirikiri.common.config;
 
-import org.springframework.beans.factory.annotation.Value;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import java.time.Duration;
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.cache.RedisCacheManager;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
-import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializationContext;
+import org.springframework.data.redis.serializer.RedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
+@EnableCaching
+@Profile(value = {"prod", "dev", "local"})
 public class RedisConfig {
 
-    @Value("${spring.data.redis.host}")
-    private String host;
-
-    @Value("${spring.data.redis.port}")
-    private int port;
-
     @Bean
-    public RedisConnectionFactory redisConnectionFactory() {
-        return new LettuceConnectionFactory(host, port);
-    }
-
-    @Bean
-    public RedisTemplate<String, String> redisTemplate() {
+    public RedisTemplate<String, String> redisTemplate(final RedisConnectionFactory redisConnectionFactory) {
         final RedisTemplate<String, String> redisTemplate = new RedisTemplate<>();
-        redisTemplate.setConnectionFactory(redisConnectionFactory());
+        redisTemplate.setConnectionFactory(redisConnectionFactory);
         redisTemplate.setKeySerializer(new StringRedisSerializer());
         redisTemplate.setValueSerializer(new StringRedisSerializer());
         return redisTemplate;
+    }
+
+    @Bean
+    public CacheManager redisCacheManager(final RedisConnectionFactory redisConnectionFactory) {
+        final ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.registerModule(new JavaTimeModule());
+        objectMapper.activateDefaultTyping(objectMapper.getPolymorphicTypeValidator(), ObjectMapper.DefaultTyping.EVERYTHING);
+
+        final RedisSerializer<Object> serializer = new GenericJackson2JsonRedisSerializer(objectMapper);
+
+        final RedisCacheConfiguration redisCacheConfiguration = RedisCacheConfiguration.defaultCacheConfig()
+                .entryTtl(Duration.ofMinutes(30L))
+                .disableCachingNullValues()
+                .serializeValuesWith(RedisSerializationContext.SerializationPair.fromSerializer(serializer));
+
+        return RedisCacheManager.RedisCacheManagerBuilder
+                .fromConnectionFactory(redisConnectionFactory)
+                .cacheDefaults(redisCacheConfiguration)
+                .build();
     }
 }
--- backend/kirikiri/src/main/java/co/kirikiri/persistence/auth/RefreshTokenRepository.java
+++ backend/kirikiri/src/main/java/co/kirikiri/persistence/auth/RefreshTokenRepository.java
@@ -1,35 +1,10 @@
 package co.kirikiri.persistence.auth;
 
 import java.util.Optional;
-import java.util.concurrent.TimeUnit;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.stereotype.Repository;
 
-@Repository
-public class RefreshTokenRepository {
+public interface RefreshTokenRepository {
 
-    private final RedisTemplate<String, String> redisTemplate;
-    private final Long refreshTokenValidityInSeconds;
+    void save(final String refreshToken, final String memberIdentifier);
 
-    public RefreshTokenRepository(final RedisTemplate<String, String> redisTemplate,
-                                  @Value("${jwt.refresh-token-validity-in-seconds}") final Long refreshTokenValidityInSeconds) {
-        this.redisTemplate = redisTemplate;
-        this.refreshTokenValidityInSeconds = refreshTokenValidityInSeconds;
-    }
-
-    public void save(final String refreshToken, final String memberIdentifier) {
-        final long timeToLiveSeconds = refreshTokenValidityInSeconds / 1000;
-
-        redisTemplate.opsForValue()
-                .set(refreshToken, memberIdentifier, timeToLiveSeconds, TimeUnit.SECONDS);
-    }
-
-    public Optional<String> findMemberIdentifierByRefreshToken(final String refreshToken) {
-        final String memberIdentifier = redisTemplate.opsForValue().get(refreshToken);
-        if (memberIdentifier == null) {
-            return Optional.empty();
-        }
-        return Optional.of(memberIdentifier);
-    }
+    Optional<String> findMemberIdentifierByRefreshToken(final String refreshToken);
 }
