53ea50f3e7c1e5a4d2490b21c654e93a9c6fc43d
YoungHoon Choi <90854702+younghoondoodoom@users.noreply.github.com>
2023-12-03 17:50:13 +0900

    [feat/CK-239] Amazon S3 접근 시 credential 정보를 이용하도록 수정한다 (#201)
    * feat: amazon s3 접근 시 credentials 사용하도록 수정
    
    * test: test application.yml에 credentials 추가

--- backend/kirikiri/src/main/java/co/kirikiri/common/config/AWSConfig.java
+++ backend/kirikiri/src/main/java/co/kirikiri/common/config/AWSConfig.java
@@ -1,5 +1,7 @@
 package co.kirikiri.common.config;
 
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
 import com.amazonaws.regions.Regions;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
@@ -11,15 +13,23 @@ import org.springframework.context.annotation.Configuration;
 public class AWSConfig {
 
     private final Regions region;
+    private final String accessKey;
+    private final String secretKey;
 
-    public AWSConfig(@Value("${cloud.aws.region.static}") final String region) {
+    public AWSConfig(@Value("${cloud.aws.region.static}") final String region,
+                     @Value("${cloud.aws.credentials.access-key}") final String accessKey,
+                     @Value("${cloud.aws.credentials.secret-key}") final String secretKey) {
         this.region = Regions.fromName(region);
+        this.accessKey = accessKey;
+        this.secretKey = secretKey;
     }
 
     @Bean
     public AmazonS3 amazonS3() {
+        final BasicAWSCredentials awsCredentials = new BasicAWSCredentials(accessKey, secretKey);
         return AmazonS3ClientBuilder.standard()
                 .withRegion(region)
+                .withCredentials(new AWSStaticCredentialsProvider(awsCredentials))
                 .build();
     }
 }
