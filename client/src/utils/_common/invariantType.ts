0044a56f0c3101a6882f89deb91fbf91dab13b5f
NaveOWO <87578512+NaveOWO@users.noreply.github.com>
2023-12-27 03:24:05 +0900

    feat: invariantType 함수 추가

--- client/src/utils/_common/invariantType.ts
+++ client/src/utils/_common/invariantType.ts
@@ -23,3 +23,13 @@ export function invariantOf<T>(value: T): InvariantOf<T> {
 export function getInvariantObjectKeys<T>(arg: InvariantOf<T>): (keyof T)[] {
   return Object.keys(arg) as unknown as (keyof T)[];
 }
+
+export function getInvariantObjectValues<T>(arg: InvariantOf<T>): T[keyof T][] {
+  return Object.values(arg) as unknown as T[keyof T][];
+}
+
+export function getInvariantObjectEntries<T>(
+  arg: InvariantOf<T>
+): [keyof T, T[keyof T]][] {
+  return Object.entries(arg) as unknown as [keyof T, T[keyof T]][];
+}
