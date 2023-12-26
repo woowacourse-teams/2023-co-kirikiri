e2fd3476f93b34b9b1e4e530c8967a2d47dda216
NaveOWO <87578512+NaveOWO@users.noreply.github.com>
2023-12-27 00:48:46 +0900

    refactor: goalroom filter select컴포넌트 유틸 라이브러리 적용

--- client/src/components/goalRoomListPage/goalRoomList/GoalRoomList.tsx
+++ client/src/components/goalRoomListPage/goalRoomList/GoalRoomList.tsx
@@ -2,19 +2,19 @@ import * as S from './goalRoomList.styles';
 import GoalRoomItem from './GoalRoomItem';
 import { useGoalRoomList } from '@/hooks/queries/goalRoom';
 import useValidParams from '@/hooks/_common/useValidParams';
-import GoalRoomFilter from './GoalRoomFilter';
-import { Select } from '@/components/roadmapCreatePage/selector/SelectBox';
 import { useState } from 'react';
 import { FILTER_COND, goalRoomFilter } from '@/constants/goalRoom/goalRoomFilter';
 import { useInfiniteScroll } from '@hooks/_common/useInfiniteScroll';
 import WavyLoading from '@components/_common/wavyLoading/WavyLoading';
 import { Link } from 'react-router-dom';
+import GoalRoomFilter from './GoalRoomFilter';
 
 const GoalRoomList = () => {
   const { id } = useValidParams<{ id: string }>();
   const [sortedOption, setSortedOption] = useState<
     (typeof goalRoomFilter)[keyof typeof goalRoomFilter]
   >(goalRoomFilter['1']);
+
   const {
     goalRoomListResponse: { responses: goalRoomList, hasNext },
     fetchNextPage,
@@ -33,6 +33,10 @@ const GoalRoomList = () => {
     (goalRoomInfo) => goalRoomInfo.status === 'RECRUITING'
   );
 
+  const selectOption = (option: keyof typeof goalRoomFilter) => {
+    setSortedOption(goalRoomFilter[option]);
+  };
+
   return (
     <S.ListContainer role='main' aria-label='골룸 리스트'>
       <S.FilterBar>
@@ -44,23 +48,7 @@ const GoalRoomList = () => {
           }
           개
         </p>
-        <GoalRoomFilter>
-          {(selectedOption) => {
-            setSortedOption(selectedOption);
-            return (
-              <Select.OptionGroup asChild>
-                <S.FilterOptionWrapper>
-                  <Select.Option id={1} asChild>
-                    <S.FilterOption>{goalRoomFilter['1']}</S.FilterOption>
-                  </Select.Option>
-                  <Select.Option id={2} asChild>
-                    <S.FilterOption>{goalRoomFilter['2']}</S.FilterOption>
-                  </Select.Option>
-                </S.FilterOptionWrapper>
-              </Select.OptionGroup>
-            );
-          }}
-        </GoalRoomFilter>
+        <GoalRoomFilter sortedOption={sortedOption} selectOption={selectOption} />
       </S.FilterBar>
       {RecruitingGoalRoomList.length ? (
         <S.ListWrapper aria-label='골룸 리스트'>
