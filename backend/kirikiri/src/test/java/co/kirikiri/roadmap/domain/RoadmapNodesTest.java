5cf81139eb7cda3a2795a9a94a5416f334a8e475
Ohjintaek <jt950930@gmail.com>
2024-01-21 19:04:54 +0900

    test: RoadmapNodes 테스트코드 추가

--- backend/kirikiri/src/test/java/co/kirikiri/roadmap/domain/RoadmapNodesTest.java
+++ backend/kirikiri/src/test/java/co/kirikiri/roadmap/domain/RoadmapNodesTest.java
@@ -4,9 +4,11 @@ import co.kirikiri.roadmap.domain.exception.RoadmapException;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
+import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertAll;
 
 class RoadmapNodesTest {
 
@@ -23,7 +25,45 @@ class RoadmapNodesTest {
                 .hasMessage("한 로드맵에 같은 이름의 노드가 존재할 수 없습니다.");
     }
 
-    // todo: RoadmapNodes 도메인 테스트 추가하기
+    @Test
+    void 로드맵_노드_아이디로_노드를_반환한다() {
+        // given
+        final RoadmapNode node1 = new RoadmapNode(1L, "title1", "content1");
+        final RoadmapNode node2 = new RoadmapNode(2L, "title2", "content2");
+        final RoadmapNodes roadmapNodes = new RoadmapNodes(List.of(node1, node2));
+
+        // when
+        final long findNodeId = 1;
+        final long notExistId = 3;
+        final Optional<RoadmapNode> foundNode1 = roadmapNodes.findById(findNodeId);
+        final Optional<RoadmapNode> foundNode2 = roadmapNodes.findById(notExistId);
+
+        // then
+        assertAll(
+                () -> assertThat(node1).isEqualTo(foundNode1.get()),
+                () -> assertThat(foundNode2).isEmpty()
+        );
+    }
+
+    @Test
+    void 로드맵_노드_제목으로_노드를_반환한다() {
+        // given
+        final RoadmapNode node1 = new RoadmapNode(1L, "title1", "content1");
+        final RoadmapNode node2 = new RoadmapNode(2L, "title2", "content2");
+        final RoadmapNodes roadmapNodes = new RoadmapNodes(List.of(node1, node2));
+
+        // when
+        final String findNodeTitle = "title1";
+        final String notExistTitle = "nothing";
+        final Optional<RoadmapNode> foundNode1 = roadmapNodes.findByTitle(findNodeTitle);
+        final Optional<RoadmapNode> foundNode2 = roadmapNodes.findByTitle(notExistTitle);
+
+        // then
+        assertAll(
+                () -> assertThat(node1).isEqualTo(foundNode1.get()),
+                () -> assertThat(foundNode2).isEmpty()
+        );
+    }
 
     @Test
     void 로드맵_노드를_추가한다() {
