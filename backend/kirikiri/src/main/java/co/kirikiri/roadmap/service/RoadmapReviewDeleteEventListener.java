3a7e610cc3104d5a8b95ff4e08c43d2e319024cd
Ohjintaek <jt950930@gmail.com>
2024-03-27 23:41:13 +0900

    refactor: 불필요한 이벤트 호출 제거

--- backend/kirikiri/src/main/java/co/kirikiri/roadmap/service/RoadmapReviewDeleteEventListener.java
+++ backend/kirikiri/src/main/java/co/kirikiri/roadmap/service/RoadmapReviewDeleteEventListener.java
@@ -3,7 +3,6 @@ package co.kirikiri.roadmap.service;
 import co.kirikiri.roadmap.persistence.RoadmapReviewRepository;
 import co.kirikiri.roadmap.service.event.RoadmapDeleteEvent;
 import lombok.RequiredArgsConstructor;
-import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Service;
 
@@ -12,15 +11,9 @@ import org.springframework.stereotype.Service;
 public class RoadmapReviewDeleteEventListener {
 
     private final RoadmapReviewRepository roadmapReviewRepository;
-    private final ApplicationEventPublisher applicationEventPublisher;
 
     @EventListener(condition = "#roadmapDeleteEvent.target == 'Review'")
     public void handleRoadmapReviewDelete(final RoadmapDeleteEvent roadmapDeleteEvent) {
-        deleteRoadmapReviews(roadmapDeleteEvent.roadmapId());
-    }
-
-    private void deleteRoadmapReviews(final Long roadmapId) {
-        roadmapReviewRepository.deleteAllByRoadmapId(roadmapId);
-        applicationEventPublisher.publishEvent(new RoadmapDeleteEvent(roadmapId, "Roadmap"));
+        roadmapReviewRepository.deleteAllByRoadmapId(roadmapDeleteEvent.roadmapId());
     }
 }
